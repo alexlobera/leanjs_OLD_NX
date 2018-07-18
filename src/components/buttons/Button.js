5abd08bf848cf838147c92da39f69ce441a75abc
alexlbr <alex.lobera@gmail.com>
2018-07-18 13:37:05 +0100

    refactores StyleButtonFontSize

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,25 +1,19 @@
 import { Button, ButtonOutline } from 'rebass'
 import styled from 'styled-components'
 
+const StyleButtonFontSize = props => {
+  if (props.extraLarge) {
+    return 20
+  } else if (props.large) {
+    return 16
+  } else {
+    return 12
+  }
+}
+
 export default styled(Button)`
-  font-size: ${props => {
-    if (props.extraLarge) {
-      return 20
-    } else if (props.large) {
-      return 16
-    } else {
-      return 12
-    }
-  }}px;
+  font-size: ${StyleButtonFontSize}px;
 `
 export const ButtonSecondary = styled(ButtonOutline)`
-  font-size: ${props => {
-    if (props.extraLarge) {
-      return 20
-    } else if (props.large) {
-      return 16
-    } else {
-      return 12
-    }
-  }}px;
+  font-size: ${StyleButtonFontSize}px;
 `
