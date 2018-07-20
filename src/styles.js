c17341044c7481c77a87633c92c981ff58271ee3
alexlbr <alex.lobera@gmail.com>
2018-07-20 18:06:29 +0100

    refactores FONT_FAMILY

--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,9 @@
+/*
+Barlow:
+900
+*/
+const FONT_FAMILY = "font-family: 'Barlow', sans-serif;"
+
 export const reactBlue = opacity => `rgba(111, 207, 240, ${opacity});` // #6FCFF0
 
 export const blue1 = opacity => `rgba(31,50,58, ${opacity});` // #1f323a
