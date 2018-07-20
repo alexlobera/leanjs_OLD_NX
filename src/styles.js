bf35cb2489ff0e94b7649acee2f75ecc7e31710b
alexlbr <alex.lobera@gmail.com>
2018-07-20 17:27:28 +0100

    adds colours to style

--- src/styles.js
+++ src/styles.js
@@ -1,1 +1,3 @@
-export const REACT_BLUE_COLOR = 'rgba(111, 207, 240, 0.14);'
+export const reactBlueColour = opacity => `rgba(111, 207, 240, ${opacity});`
+
+export const darkGreenColour = opacity => `rgba(31,50,58, ${opacity});`
