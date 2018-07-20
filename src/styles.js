850b384c14a190af216863da5e2f620eec916e7d
Tiago Gomes <t.dfgomes@icloud.com>
2018-07-20 18:11:42 +0100

    fix svg attribute and add dfault value to the color functions

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
-export const reactBlue = opacity => `rgba(111, 207, 240, ${opacity});` // #6FCFF0
+export const reactBlue = (opacity = 1) => `rgba(111, 207, 240, ${opacity});` // #6FCFF0
 
-export const blue1 = opacity => `rgba(31,50,58, ${opacity});` // #1f323a
+export const blue1 = (opacity = 1) => `rgba(31,50,58, ${opacity});` // #1f323a
 
 export const BLUE_3 = '#367088'
 
