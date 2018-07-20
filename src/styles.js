710955de759ca582895756bb526359010a07804f
Richard Moss <richard@englishtutorslive.com>
2018-07-20 17:43:32 +0100

    Update button and add default colors

--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,7 @@
 export const reactBlueColour = opacity => `rgba(111, 207, 240, ${opacity});`
 
 export const darkGreenColour = opacity => `rgba(31,50,58, ${opacity});`
+
+export const BLUE2 = '#002938'
+
+export const CALLTOACTIONRED = '#be6045'
