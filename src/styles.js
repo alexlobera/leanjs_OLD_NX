8103c5361274940abd408d7e5d5feea1acdca9f5
alexlbr <alex.lobera@gmail.com>
2018-07-20 18:51:40 +0100

    rename blue 3

--- src/styles.js
+++ src/styles.js
@@ -4,12 +4,14 @@ Barlow:
 */
 const FONT_FAMILY = "font-family: 'Barlow', sans-serif;"
 
-export const reactBlue = opacity => `rgba(111, 207, 240, ${opacity});` // #6FCFF0
+export const reactBlue = (opacity = 1) => `rgba(111, 207, 240, ${opacity});` // #6FCFF0
 
 export const blue1 = (opacity = 1) => `rgba(31,50,58, ${opacity});` // #1f323a
 
-export const BLUE_3 = '#367088'
+export const BLUE3 = '#367088'
 
 export const BLUE2 = '#002938'
+
+export const blue4 = (opacity = 1) => `rgba(87,178,212, ${opacity})`
 
 export const CALLTOACTIONRED = '#be6045'
