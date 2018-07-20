7db525c9105bed8c73b521f4fd1e1fd92e7f85d0
Richard Moss <richard@englishtutorslive.com>
2018-07-20 18:38:14 +0100

    Update Secondary Button

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, ButtonOutline } from 'rebass'
 import styled from 'styled-components'
-import { BLUE2, CALLTOACTIONRED } from '../../styles'
+import { BLUE2, CALLTOACTIONRED, FONT_FAMILY } from '../../styles'
 
 const StyleButtonFontSize = props => {
   if (props.extraLarge) {
@@ -19,7 +19,7 @@ export default styled(({ extraLarge, large, ...props }) => (
   font-size: ${StyleButtonFontSize}px;
   background-color: ${props => (props.cta ? CALLTOACTIONRED : BLUE2)};
   border-radius: 0;
-  font-family: Barlow;
+  ${FONT_FAMILY}
   font-weight: ${props => (props.cta ? 'bold' : '')};
   font-style: normal;
   font-stretch: normal;
@@ -33,4 +33,11 @@ export const ButtonSecondary = styled(({ extraLarge, large, ...props }) => (
   <ButtonOutline {...props} />
 ))`
   font-size: ${StyleButtonFontSize}px;
+  width: 223px;
+  height: 58px;
+  border-radius: 2px;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.45), 0 0 2px 0 rgba(0, 0, 0, 0.12);
+  background-color: #ffffff;
+  border: solid 1px #002938;
+  color: ${BLUE2};
 `
