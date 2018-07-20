00cb5675926d90a9d332bf48cc7dbe1e3f9f9089
alexlbr <alex.lobera@gmail.com>
2018-07-20 19:24:00 +0100

    refactores Section component

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Section = styled.section`
   padding-top: ${props => (props.top ? '150px' : '60px')};
-  ${props => (props.top ? 'margin-top: -85px' : '')} padding-bottom: 60px;
+  ${props => (props.top ? 'margin-top: -85px;' : '')} padding-bottom: 60px;
   background-color: ${props => {
     switch (props.color) {
       case 'lightGrey':
