251388e963f744e6e5cc400306944fb9d02bb653
Tiago Gomes <t.dfgomes@icloud.com>
2018-07-21 10:48:42 +0100

    add last svg company logo

---
+++ src/components/logos/Trainline.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Trainline from './Trainline'
+
+storiesOf('Companies Logos', module).add('Trainline Logo', () => <Trainline />)
