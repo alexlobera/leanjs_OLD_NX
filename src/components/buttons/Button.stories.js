bb8bf1e83d7a796dca1adf3debdd72e56bb7869f
alexlbr <alex.lobera@gmail.com>
2018-07-23 16:40:49 +0100

    removes SecondaryButton from storyborad

--- src/components/buttons/Button.stories.js
+++ src/components/buttons/Button.stories.js
@@ -5,6 +5,3 @@ import Button, { ButtonSecondary } from './Button'
 storiesOf('Button', module)
   .add('Default', () => <Button>Click me</Button>)
   .add('Call to action', () => <Button cta>Call to action</Button>)
-  .add('Secondary button', () => (
-    <ButtonSecondary large>Secondary button>></ButtonSecondary>
-  ))
