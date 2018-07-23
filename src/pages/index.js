1c57af86ef9f755df62fe936c5c41e1f2519816a
Richard Moss <richard@englishtutorslive.com>
2018-07-23 18:38:14 +0100

    Completes section image imports for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,6 +43,8 @@ import {
 import london from '../../img/london.png'
 import lisbon from '../../img/lisbon.png'
 import courseCollab from '../../img/courseCollab.png'
+import whyReactJSAcademy from '../../img/whyReactJSAcademy.png'
+import companyPitch from '../../img/companyPitch.png'
 
 const SectionButtonRow = styled(Row)`
   margin-top: 30px;
@@ -106,7 +108,7 @@ const IndexPage = () => (
             <Row>
               <DisplaySingleComponentUsingCss xs sm>
                 <Col xs={5}>
-                  <ImagePlaceholder />
+                  <img src={courseCollab} width="100%" />
                 </Col>
               </DisplaySingleComponentUsingCss>
               <Col xs={7} md={12} lg={11} lgOffset={1}>
@@ -176,7 +178,7 @@ const IndexPage = () => (
             <Row>
               <DisplaySingleComponentUsingCss xs sm>
                 <Col xs={5}>
-                  <ImagePlaceholder width="100%" height="500px" />
+                  <img src={whyReactJSAcademy} width="100%" />
                 </Col>
               </DisplaySingleComponentUsingCss>
               <Col xs={7} md={12}>
@@ -233,7 +235,7 @@ const IndexPage = () => (
           </Col>
           <HideSingleComponentUsingCss xs sm>
             <Col md={5}>
-              <ImagePlaceholder width="100%" height="500px" />
+              <img src={whyReactJSAcademy} width="100%" />
             </Col>
           </HideSingleComponentUsingCss>
         </Row>
@@ -245,7 +247,7 @@ const IndexPage = () => (
         <Row>
           <HideSingleComponentUsingCss xs sm>
             <Col md={5}>
-              <ImagePlaceholder width="100%" height="500px" />
+              <img src={companyPitch} width="100%" />
             </Col>
           </HideSingleComponentUsingCss>
           <Col md={7}>
@@ -260,7 +262,7 @@ const IndexPage = () => (
             <Row>
               <DisplaySingleComponentUsingCss xs sm>
                 <Col xs={5}>
-                  <ImagePlaceholder width="100%" height="500px" />
+                  <img src={companyPitch} width="100%" />
                 </Col>
               </DisplaySingleComponentUsingCss>
               <Col xs={7} md={12} lg={10} lgOffset={1}>
