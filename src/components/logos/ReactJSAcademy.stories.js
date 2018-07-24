5d1c60e191fdb142b052e8375ba4235e7c7e6c9c
Tiago Gomes <t.dfgomes@icloud.com>
2018-07-24 16:52:57 +0100

    remove duplicated logos

--- src/components/logos/ReactJSAcademy.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { Logo } from './ReactJSAcademy'
-
-storiesOf('ReactJS Academy Logo', module).add('Standard logo', () => <Logo />)
