d8ca8b808a99f8a510587aaf3b33f821920716e0
alexlbr <alex.lobera@gmail.com>
2018-07-26 01:31:04 +0200

    removes unused code in text and training

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -2,5 +2,3 @@ export { H1, H1Ref, H2, H2Ref, H3, H4, H5 } from './H'
 export { default as Blockquote } from './Blockquote'
 export { default as Label } from './Label'
 export { default as P } from './P'
-
-export const textSize = {}
