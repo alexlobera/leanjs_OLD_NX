ca6d6a3e53e8daebe45028554af05bd834fe41b1
alexlbr <alex.lobera@gmail.com>
2018-07-26 13:42:34 +0200

    removes unused imports src/pages/react-redux-graphql-bootcamp-austin.js

--- src/pages/react-redux-graphql-bootcamp-austin.js
+++ src/pages/react-redux-graphql-bootcamp-austin.js
@@ -1,50 +1,20 @@
 import React from 'react'
-import styled from 'styled-components'
 import Section, { TopSection } from '../components/layout/Section'
-import { Button, LinkButton } from '../components/buttons'
 import Grid, { Col, Row } from '../components/layout/Grid'
-import { H2, H2Ref, H3, P } from '../components/text'
+import { H2, H3, P } from '../components/text'
 import Ul, { Li } from '../components/layout/Ul'
 import { CurriculumBootcamp } from '../components/curriculum'
-import { Ribbon, Card, Video } from '../components/elements'
-import Link from '../components/navigation/Link'
-import { HideSingleComponentUsingCss } from '../components/utils'
+import { Card, Video } from '../components/elements'
 import Header from '../components/layout/Header'
-import { GREY2, FONT_FAMILY } from '../config/styles'
-import { LONDON, BOOTCAMP_COLLAB, CATALIN } from '../config/images'
 import { TrustedByLogoList } from '../components/training/TrustedBySection'
 import CallToActionRow from '../components/layout/CallToActionRow'
-import { UpcomingBootcampsSection, AttendeeQuote } from '../components/training'
-import {
-  BulletIcon,
-  NotBegginerIcon,
-  CodeIcon,
-  ReactIcon,
-  CollabsIcon,
-} from '../components/icons'
-import { Image } from '../components/elements'
-import BuyQuantityButton from '../components/old_checkout/containers/PurchaseQuantityContainer'
-import header from '../components/layout/Header.json'
+import { UpcomingBootcampsSection } from '../components/training'
 
-const Price = styled.span`
-  ${FONT_FAMILY} font-size: 36px;
-  font-weight: 800;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: 1.5;
-  letter-spacing: normal;
-  color: ${GREY2};
-`
-
-const InstallmentsContainer = styled.div`
-  margin: 12px 0;
-`
-
-const BootcampLondon = () => (
+const BootcampAustin = () => (
   <React.Fragment>
     <Header
       titleLines={['React Redux GraphQL Bootcamp', 'Training in Austin']}
-      subtitle="Take your dev career to the next level in Austin by mastering<br />React, Redux, and GraphQL - in just 7 days!"
+      subtitle="Take your dev career to the next level in Austin -Texas- by mastering<br />React, Redux, and GraphQL - in just 7 days!"
       bgImg="training-event"
     />
     <TopSection>
@@ -69,32 +39,14 @@ const BootcampLondon = () => (
             <Col xs={12} md={6} lg={5} lgOffset={1}>
               <P>
                 ADD SOME COOL TEXT with: - city, state or country - react,
-                redux, graphql
+                redux, graphql - shall we mention Kyle simpson partnership to
+                deliver this?
               </P>
             </Col>
           </Row>
         </Card>
       </Grid>
     </Section>
-    {/*
-
-            SHOULD WE ADD A QUOTE? WE DON'T WANT TO DUPLICATE CONTENT FOR SEO
-
-        <Section>
-            <Grid>
-                <Row>
-                    <Col lg={10} lgOffset={1}>
-                        <AttendeeQuote
-                            quote="Technology nowadays changes very often and in future you may not be able to find a job with the things you know - you have to keep up. I like the fact that we got to write code rather than focus on theory - it’s very balanced."
-                            fullname="Catalin Cislariu"
-                            job="Senior Developer"
-                            company="KLEIDO LTD"
-                            profilePicUrl={CATALIN}
-                        />
-                    </Col>
-                </Row>
-            </Grid>
-        </Section> */}
     <Section>
       <Grid>
         <Card white border="shadow">
@@ -107,4 +59,4 @@ const BootcampLondon = () => (
   </React.Fragment>
 )
 
-export default BootcampLondon
+export default BootcampAustin
