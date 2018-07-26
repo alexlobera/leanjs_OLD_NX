9db62e6d23b543e19113f940a0cce1e22b79f857
Richard Moss <richard@englishtutorslive.com>
2018-07-26 14:45:09 +0100

    Add lisbon bootcamp page

---
+++ src/pages/react-redux-graphql-bootcamp-lisbon.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Section, { TopSection } from '../components/layout/Section'
+import Grid, { Col, Row } from '../components/layout/Grid'
+import { H2, H3, P } from '../components/text'
+import Ul, { Li } from '../components/layout/Ul'
+import { CurriculumBootcamp } from '../components/curriculum'
+import { Card, Video } from '../components/elements'
+import Header from '../components/layout/Header'
+import { TrustedByLogoList } from '../components/training/TrustedBySection'
+import CallToActionRow from '../components/layout/CallToActionRow'
+import { UpcomingBootcampsSection } from '../components/training'
+
+const BootcampLisbon = () => (
+  <React.Fragment>
+    <Header
+      titleLines={['React Redux GraphQL Bootcamp', 'Training in Lisbon']}
+      subtitle="Take your dev career to the next level in Lisbon -Portugal- by mastering<br />React, Redux, and GraphQL - in just 7 days!"
+      bgImg="training-event"
+    />
+    <TopSection>
+      <Grid>
+        <Card border="shadow">
+          <Row>
+            <Col xs={12} lg={10} lgOffset={1}>
+              <H2>We've trained developers from a range of companies</H2>
+              <TrustedByLogoList />
+            </Col>
+          </Row>
+        </Card>
+      </Grid>
+    </TopSection>
+    <Section xsBgDark>
+      <Grid>
+        <Card bg="dark">
+          <Row>
+            <Col xs={12} md={6} lg={4} lgOffset={1}>
+              <Video src="https://www.youtube.com/embed/yvROXLQ1jHg" />
+            </Col>
+            <Col xs={12} md={6} lg={5} lgOffset={1}>
+              <P>
+                Join us for our next addition of the React, Redux GraphQL bootcamp Lisbon.
+                ReactJS Academy has delivered 3 iterations of our Portugal based bootcamp with developers joining from over
+                12 countries from as far away as USA and Saudi Arabia!
+              </P>
+              <P>
+                Check out the 3 minute video on the left to get a feel for our bootcamps and here what some of our preivous students think about the experience :-)
+              </P>
+            </Col>
+          </Row>
+        </Card>
+      </Grid>
+    </Section>
+    <Section>
+      <Grid>
+        <Card white border="shadow">
+          <CurriculumBootcamp />
+        </Card>
+      </Grid>
+    </Section>
+
+    <UpcomingBootcampsSection />
+  </React.Fragment>
+)
+
+export default BootcampLisbon
