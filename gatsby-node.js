adcac1fb99e4de8a476c7bdff1ae03e1f8db8267
alexlbr <alex.lobera@gmail.com>
2018-07-29 17:04:46 +0200

    fixes assets cache issue on CDN

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,3 +12,19 @@ exports.modifyBabelrc = ({ babelrc }) => ({
     ['transform-runtime']
   ),
 })
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  const timestamp = Date.now()
+  switch (stage) {
+    case 'build-javascript':
+      config.merge({
+        output: {
+          filename: `[name]-${timestamp}-[chunkhash].js`,
+          chunkFilename: `[name]-${timestamp}-[chunkhash].js`
+        }
+      })
+
+      break
+  }
+  return config
+}
