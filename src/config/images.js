be34e78c884c05f2294674dd8456d42e7f702dad
Horacio Herrera <hherrerag@me.com>
2018-08-01 13:29:05 +0200

    remove bad image source

--- src/config/images.js
+++ src/config/images.js
@@ -1,62 +1,62 @@
 export const HOME_IMG =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/homepage_topPage.jpg?alt=media&token=f61650f8-d912-4e1c-8d96-a3f38633d66f'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/homepage_topPage.jpg?alt=media
 export const PART_TIME_IMG =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/partTime_splash.jpg?alt=media&token=e3ff03da-ffea-46c0-b154-52087505e405'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/partTime_splash.jpg?alt=media
 export const FULL_TIME_IMG =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/bootcamp_Splash.jpg?alt=media&token=977fbec7-d3bf-43f4-965a-65a98cd8b0d6'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/bootcamp_Splash.jpg?alt=media
 export const TRAINING_EVENT_IMG =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/londonBootcamp_splash.jpg?alt=media&token=9ef553f7-22b1-47f2-a907-566d00eaffb8'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/londonBootcamp_splash.jpg?alt=media
 export const CURRICULUM_IMG =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/curriculum_splash.jpg?alt=media&token=94eb6ead-aa35-4912-a0ee-3007119ac0e6'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/curriculum_splash.jpg?alt=media
 export const COMMUNITY_IMG =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/community_splash.jpg?alt=media&token=c50d53fc-a26a-4180-9939-313f7aaee98c'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/community_splash.jpg?alt=media
 export const LONDON =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/london.png?alt=media&token=986a4d99-5fbc-45c8-b49d-34d20088b2f8'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/london.png?alt=media
 export const LISBON =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/lisbon.png?alt=media&token=27fdef75-d3d7-48bd-bbb1-da086bccc78b'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/lisbon.png?alt=media
 export const COURSE_COLLAB =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/courseCollab.png?alt=media&token=f0455730-ee67-4da1-afb6-32d51817a2b4'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/courseCollab.png?alt=media
 export const WHY_REACTJS_ACADEMY =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/whyReactJSAcademy.png?alt=media&token=11da0b2e-4409-4bc4-86dd-124b9cd73d58'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/whyReactJSAcademy.png?alt=media
 export const COMPANY_PITCH =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/companyPitch.png?alt=media&token=fda8a9c0-e366-42cc-832c-9233aae5d38e'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/companyPitch.png?alt=media
 
 export const BOOTCAMP_RIGHT =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/bootcamps.png?alt=media&token=691dae6e-28d1-4ae7-aa93-74ddd76bd4c5'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/bootcamps.png?alt=media
 
 export const BOOTCAMP_COLLAB =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/bootcamp-collab.png?alt=media&token=1d32ed28-c7d5-4690-ac15-579fb748ef1e'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/bootcamp-collab.png?alt=media
 
 export const PART_TIME =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/part-time.png?alt=media&token=6e68aa64-945e-4268-8c53-e3200a10b441'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/part-time.png?alt=media
 
 export const ABOUT_CONTINUOUS =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/aboutUs_continuousImprove.png?alt=media&token=ad65a48b-d8ff-4164-9d35-be3c5ca1a985'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/aboutUs_continuousImprove.png?alt=media
 
 export const ABOUT_CORPORATE =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/aboutUS_corporateTraining.png?alt=media&token=468b5c08-d69e-4fdc-9080-3668a482cb00'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/aboutUS_corporateTraining.png?alt=media
 // Team members
 
 export const ALEX =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Falex.png?alt=media&token=53e46088-9e37-4a41-830b-d3d5fef801c5'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Falex.png?alt=media
 export const RICHARD =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Frichard.png?alt=media&token=417d5d3f-877c-4964-b19f-b86e2c4b18a1'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Frichard.png?alt=media
 export const HORACIO =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Fhoracio-pic.png?alt=media&token=c9e4e065-c541-4924-bae8-d9c83923c632'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Fhoracio-pic.png?alt=media
 export const WILL =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Fwill.png?alt=media&token=8ab839e8-2714-47b4-b0dc-f14cfb76c87e'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Fwill.png?alt=media
 export const ADAM =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Fadam-lancaster.jpeg?alt=media&token=1f57815b-0a6b-419a-b5af-63634ba0395f'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/team%2Fadam-lancaster.jpeg?alt=media
 
 // Tesimonials
 
 export const CATALIN =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FCatalinCislariu.png?alt=media&token=01a4217b-1385-43ab-af97-36c56044bc99'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FCatalinCislariu.png?alt=media
 export const DAN =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FDanJones.png?alt=media&token=00e3410a-9bb1-4670-a0ef-49a7bc5f6cad'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FDanJones.png?alt=media
 export const NASSAR =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FNassarKasirye.png?alt=media&token=100fb610-183b-4166-8c38-bc06e4c53369'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FNassarKasirye.png?alt=media
 export const OLU =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FOluOmoniyi.png?alt=media&token=c331eba9-1339-4db0-a697-04d3ff66f4f3'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FOluOmoniyi.png?alt=media
 export const STEFANO =
-  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FStefanoMezza.png?alt=media&token=a069416d-2c6b-4937-bb2c-089956bea663'
+  'https://firebasestorage.googleapis.com/v0/b/reactjsacademy-react.appspot.com/o/testimonials%2FStefanoMezza.png?alt=media
