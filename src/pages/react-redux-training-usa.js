d7f6fbf84c3ace7023b9d7e5ae41bd4d5ccd8c88
alexlbr <alex.lobera@gmail.com>
2018-08-08 16:22:52 +0100

    refactores client redirects onto server redirects

--- src/pages/react-redux-training-usa.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import RedirectPage from '../components/utils/RedirectPage'
-
-const Redirect = () => (
-  <RedirectPage to="https://reactjs.academy/react-redux-graphql-bootcamp-nyc/" />
-)
-
-export default Redirect
