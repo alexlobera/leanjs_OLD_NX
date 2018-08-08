ff4727746728197d396d788655208c6ab79d467a
alexlbr <alex.lobera@gmail.com>
2018-08-08 15:02:33 +0100

    adds redirect from training usa to bootcamp nyc

---
+++ src/pages/react-redux-training-usa.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import RedirectPage from '../components/utils/RedirectPage'
+
+const Redirect = () => (
+  <RedirectPage to="https://reactjs.academy/react-redux-graphql-bootcamp-nyc/" />
+)
+
+export default Redirect
