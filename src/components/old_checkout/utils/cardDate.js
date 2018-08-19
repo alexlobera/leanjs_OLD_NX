bd0cd703400149f06ce81b42b13f1d6a3544f421
alexlbr <alex.lobera@gmail.com>
2018-08-19 16:13:36 +0100

    deletes old checkout

--- src/components/old_checkout/utils/cardDate.js
+++
@@ -1,8 +0,0 @@
-export const getMonthFromCardDate = cardDate =>
-  parseInt(cardDate.substring(0, cardDate.lastIndexOf('/')), 10)
-
-export const getYearFromCardDate = cardDate =>
-  parseInt(
-    cardDate.substring(cardDate.lastIndexOf('/') + 1, cardDate.length),
-    10
-  )
--- src/components/old_checkout/utils/phone.js
+++
@@ -1,6 +0,0 @@
-const isPhone = () =>
-  typeof window !== 'undefined' &&
-  window.innerWidth <= 800 &&
-  window.innerHeight <= 600
-
-export default isPhone
