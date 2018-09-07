abc2f0e870049be85d3abdd0c3dbef38bcaf34e1
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-09-07 20:12:15 +0100

    Removing duplicate code

--- src/components/utils/aliasComponent.js
+++ src/components/utils/aliasComponent.js
@@ -5,4 +5,5 @@ import React from 'react'
 // easily be provided as a selector to Enzyme wrapper.find() to hook in for integration testing.
 export const aliasComponent = Component => props => <Component {...props} />
 
+// This function is for when you want to alias the same component multiple times in the same scope
 export const getComponentAliaser = Component => () => aliasComponent(Component)
