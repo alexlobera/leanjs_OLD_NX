3011d47f0588f78b321bae4f78df74393d244fd6
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-09-07 20:13:40 +0100

    Improving comment

--- src/components/utils/aliasComponent.js
+++ src/components/utils/aliasComponent.js
@@ -6,4 +6,5 @@ import React from 'react'
 export const aliasComponent = Component => props => <Component {...props} />
 
 // This function is for when you want to alias the same component multiple times in the same scope
+// (you can assign the output, which is a function, to a constant and call that function each time you need to create a new alias)
 export const getComponentAliaser = Component => () => aliasComponent(Component)
