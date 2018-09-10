cd2abdc83163dec46fcb3637e454b621e5c6fd1b
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-09-10 18:40:44 +0100

    Adding tests for getUrlParam

--- src/components/utils/url.test.js
+++ src/components/utils/url.test.js
@@ -2,23 +2,26 @@
 import { getURLParameter } from './url'
 
 describe('getURLParameter', () => {
-	it('should decode URI components', () => {
 
+	beforeAll(() => {
+		jsdom.reconfigure({ url: "https://www.example.com/foo/bar/?a=b%23&x=y+z&c=d%20ef%20g&i=jk;lm" });
 	})
-	it('should extract parameters from the query string ', () => {
-		
-	})
-	it('should not extract parameters from the path ', () => {
-		
+
+	it('should decode URI components', () => {
+		expect(getURLParameter('a')).toBe('b#')
 	})
 	it('should interpret the + sign as a space', () => {
-		
+		expect(getURLParameter('x')).toBe('y z')
 	})
 	it('should interpret %20 as a space', () => {
-		
+		expect(getURLParameter('c')).toBe('d ef g')
 	})
-	it('should not match any of the following characters: [&;]', () => {
-		
+	it('should not match a semi-colon character', () => {
+		expect(getURLParameter('i')).toBe('jk')	
+	})
+	it('should return null if there is no query string', () => {
+		jsdom.reconfigure({ url: "https://www.example.com/foo/bar/" });
+		expect(getURLParameter('a')).toBe(null)
 	})
 })
 
