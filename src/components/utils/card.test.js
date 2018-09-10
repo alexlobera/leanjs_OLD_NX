204348b35ff7be2ba97b66b0196b40d9d92984b1
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-09-10 11:21:51 +0100

    Adding tests for card date utils

---
+++ src/components/utils/card.test.js
@@ -0,0 +1,78 @@
+import Payment from 'payment'
+
+import {
+  getMonthFromCardDate,
+  getYearFromCardDate,
+} from './card.js'
+
+describe('getMonthFromCardDate', () => {
+  it('should extract the month from a date in the format MM/YY', () => {
+    expect(getMonthFromCardDate("16/19")).toBe(16)
+  })
+})
+
+describe('getYearFromCardDate', () => {
+  it('should extract the year from a date in the format MM/YY', () => {
+    expect(getYearFromCardDate("16/19")).toBe(19)
+  })
+  it('should strip any non-numberic characters from around the year', () => {
+    expect(getYearFromCardDate("16/-19abc:")).toBe(19)
+  })
+})
+
+/*
+export function formatCreditCardNumber(value) {
+  if (!value) {
+    return ''
+  }
+
+  const issuer = Payment.fns.cardType(value)
+  const clearValue = clearNumber(value)
+  let nextValue
+
+  switch (issuer) {
+    case 'amex':
+      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
+        4,
+        10
+      )} ${clearValue.slice(10, 15)}`
+      break
+    case 'dinersclub':
+      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
+        4,
+        10
+      )} ${clearValue.slice(10, 14)}`
+      break
+    default:
+      nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
+        4,
+        8
+      )} ${clearValue.slice(8, 12)} ${clearValue.slice(12, 19)}`
+      break
+  }
+
+  return nextValue.trim()
+}
+
+export function formatCVC(value, allValues = {}) {
+  const clearValue = clearNumber(value)
+  let maxLength = 4
+
+  if (allValues.number) {
+    const issuer = Payment.fns.cardType(allValues.number)
+    maxLength = issuer === 'amex' ? 4 : 3
+  }
+
+  return clearValue.slice(0, maxLength)
+}
+
+export function formatExpirationDate(value) {
+  const clearValue = clearNumber(value)
+
+  if (clearValue.length >= 3) {
+    return `${clearValue.slice(0, 2)} / ${clearValue.slice(2, 4)}`
+  }
+
+  return clearValue
+}
+*/
