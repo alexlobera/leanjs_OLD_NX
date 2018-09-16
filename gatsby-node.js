53c4775c6ef3d6a54a86bb851fc840ed36810e3b
Richard Moss <richard@englishtutorslive.com>
2018-09-15 20:10:33 -0700

    Add sample .md and implement onCreateNode api

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,20 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
 const Webpack = require('webpack')
 
 exports.modifyBabelrc = ({ babelrc }) => ({
@@ -17,14 +31,14 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
   const timestamp = Date.now()
   config.loader('graphql-tag/loader', {
     test: /\.(graphql|gql)$/,
-  });
+  })
   switch (stage) {
     case 'build-javascript':
       config.merge({
         output: {
           filename: `[name]-${timestamp}-[chunkhash].js`,
-          chunkFilename: `[name]-${timestamp}-[chunkhash].js`
-        }
+          chunkFilename: `[name]-${timestamp}-[chunkhash].js`,
+        },
       })
       break
   }
---
+++ src/pages/react-redux-training-sydney.md
@@ -0,0 +1,8 @@
+---
+
+city: Sydney
+country: Australia
+url: /react-redux-training-sydney
+---
+
+Something about the location or description etc
