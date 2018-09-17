7359cab97b54daa2d8c4c1a41539b307daf94fda
Richard Moss <richard@englishtutorslive.com>
2018-09-17 15:38:49 -0700

    Update folder structure and move .md files into sub-directoty

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,11 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: `pages/locations-landing-content`,
+    })
     createNodeField({
       node,
       name: `slug`,
