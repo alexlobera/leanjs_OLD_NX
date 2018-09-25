b8864e7f9499ac7a16d74e7c00ba2b3549c957f0
Richard Moss <richard@englishtutorslive.com>
2018-09-24 19:28:31 -0700

    Add basic test for filtering past trainings

--- src/config/data.test.js
+++ src/config/data.test.js
@@ -4,10 +4,11 @@ import {
   ADVANCED_REACT,
   REACT_BOOTCAMP,
   LISBON,
+  REACT_NATIVE,
 } from './data'
 
 describe('selectTrainings', () => {
-  let londonBootcamp = [
+  const londonBootcamp = [
     {
       dates: '3-8 Dec, 2018',
       dateStartsOn: new Date('3 December, 2018'),
@@ -27,8 +28,30 @@ describe('selectTrainings', () => {
       currency: 'gbp',
     },
   ]
+  const pastTraining = [
+    {
+      dates: '17 September, 2018',
+      dateStartsOn: new Date('17 Sep, 2018'),
+      city: LONDON,
+      country: 'UK',
+      type: REACT_NATIVE,
+      cityShortName: 'London',
+      country: 'UK',
+      location: '8 Spital Square E1 6DU, London',
+      pathUrl: '/react-native-bootcamp-london',
+      trainingInstanceId: '5b68c95eaff9b939d15e509c',
+      price: 415.83,
+      discountPrice: 375,
+      priceGoesUpOn: 'September 14th, 2018.',
+      ticketName: 'Early bird ticket',
+      currency: 'gbp',
+    },
+  ]
   it('should return an array of trainings based on type and location', () => {
     expect(selectTrainings(REACT_BOOTCAMP, LONDON)).toEqual(londonBootcamp)
   })
-  it('should not return a training if the dateStartsOn is in the past', () => {})
+  it('should not return a training if the dateStartsOn is in the past', () => {
+    expect(selectTrainings()).toContainEqual(londonBootcamp[0])
+    expect(selectTrainings()).toContainEqual(pastTraining[0])
+  })
 })
