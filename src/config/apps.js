c0449172e2d95b8d8e3dd379b8f3af154c776b6c
alexlbr <alex.lobera@gmail.com>
2018-09-28 16:24:05 +0100

    adds .env

---
+++ src/config/apps.js
@@ -0,0 +1,9 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
+export const UPMENTORING_API_URL = isProduction
+  ? 'https://api.upmentoring.com/api/graphql'
+  : 'http://api.upmentoring.local:8080/api/graphql'
+
+export const STRIPE_PUBLIC_KEY = process.env.SENTRY_DSN
+
+export const SENTRY_DSN = process.env.STRIPE_PUBLIC_KEY
