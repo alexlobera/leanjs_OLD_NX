e8db6e189693c216d41f029bdf57181b94e3047b
alexlbr <alex.lobera@gmail.com>
2018-09-28 16:33:10 +0100

    modifies  src/config/apps.js

--- src/config/apps.js
+++ src/config/apps.js
@@ -4,6 +4,6 @@ export const UPMENTORING_API_URL = isProduction
   ? 'https://api.upmentoring.com/api/graphql'
   : 'http://api.upmentoring.local:8080/api/graphql'
 
-export const STRIPE_PUBLIC_KEY = process.env.SENTRY_DSN
+export const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY
 
-export const SENTRY_DSN = process.env.STRIPE_PUBLIC_KEY
+export const SENTRY_DSN = process.env.SENTRY_DSN
