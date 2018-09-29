f275b9f298441a3292ae9d4fab3dced467a056ed
Richard Moss <richard@englishtutorslive.com>
2018-09-29 13:00:06 -0700

    Delete unused firebase and functions folders from project

--- firebase/db.js
+++
@@ -1,9 +0,0 @@
-import { db } from './firebase'
-
-// Save message from contact form  in realtime db
-
-export const doCreateMessage = (name, email, message) =>
-  db.ref(`contactSubmissions/${name}`).set({
-    email,
-    message,
-  })
