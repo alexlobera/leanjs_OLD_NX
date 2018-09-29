90d6b0a24c6f29e6fda9df155f208cf955f128c9
Richard Moss <richard@englishtutorslive.com>
2018-09-29 14:03:51 -0700

    remove snapshot for about and update test file

--- src/pages/about-us.test.js
+++ src/pages/about-us.test.js
@@ -5,8 +5,7 @@ import { shallowToJson } from 'enzyme-to-json'
 import AboutUs from './about-us'
 
 describe('<AboutUs />', () => {
-  it('should render component', () => {
-    const wrapper = shallow(<AboutUs />)
-    expect(shallowToJson(wrapper)).toMatchSnapshot()
+  it('should render page', () => {
+    //add basic test with no snapshot
   })
 })
