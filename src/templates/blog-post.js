2f8606ba89d6387d69c64d10110d61bae0e45c85
alexlbr <alex.lobera@gmail.com>
2018-10-14 17:57:33 +0100

    fixes style post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 import Grid, { Col, Row } from '../components/layout/Grid'
-import { H3 } from '../components/text'
+import { H3, H2_STYLE, H3_STYLE, H4_STYLE, H5_STYLE, P_STYLE } from '../components/text'
+import { ANCHOR_STYLE } from '../components/navigation/'
 import Header from '../components/layout/Header'
 import { UpcomingTrainingSection } from '../components/training'
 import { Breadcrumb } from '../components/navigation'
+
+const Content = styled.div`
+  p {
+    ${P_STYLE}
+  }
+  h2 {
+    ${H2_STYLE}
+  }
+  h3 {
+    ${H3_STYLE}
+  }
+  h4 {
+    ${H4_STYLE}
+  }
+  h5 {
+    ${H5_STYLE}
+  }
+  a {
+    ${ANCHOR_STYLE}
+  }
+`
 
 const BlogPost = ({ data }) => {
   const { title, subtitle } = data.markdownRemark.frontmatter
@@ -30,7 +53,7 @@ const BlogPost = ({ data }) => {
         <Row>
           <Col md={6} mdOffset={1}>
             {subtitle ? <H3>{subtitle}</H3> : null}
-            <div dangerouslySetInnerHTML={{ __html: html }} />
+            <Content dangerouslySetInnerHTML={{ __html: html }} />
           </Col>
           <Col md={3} mdOffset={1}>
             right column
