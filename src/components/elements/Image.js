4dfcf3c71c95189f82f9098c52fde8778bd2f666
alexlbr <alex.lobera@gmail.com>
2018-10-14 18:57:11 +0100

    adds author to blog post

--- src/components/elements/Image.js
+++ src/components/elements/Image.js
@@ -4,6 +4,11 @@ import styled from 'styled-components'
 
 const Image = styled.img`
   margin: 0;
+  ${({ circle }) => circle ? `border-radius: 50%;` : null}
+  ${({ width }) => width ? `
+    width: ${width}px;
+    height: ${width}px;
+  `: null}
 `
 
 Image.propTypes = {
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import Grid, { Col, Row } from '../components/layout/Grid'
-import { H2, H2_STYLE, H3_STYLE, H4_STYLE, H5_STYLE, P_STYLE } from '../components/text'
+import { P, Span, H2, H2_STYLE, H3_STYLE, H4_STYLE, H5_STYLE, P_STYLE } from '../components/text'
 import { ANCHOR_STYLE } from '../components/navigation/'
 import Header from '../components/layout/Header'
 import { UpcomingTrainingSection } from '../components/training'
-import { Breadcrumb } from '../components/navigation'
-import { FONT_FAMILY } from '../config/styles'
+import { Breadcrumb, Link } from '../components/navigation'
+import { FONT_FAMILY, WHITE } from '../config/styles'
+import { Image } from '../components/elements'
+import {
+  RICHARD,
+  HORACIO,
+  ALEX,
+} from '../config/images'
+import { RICHARD_MOSS as RICHARD_MOSS_PATH } from '../components/training/TrainingDetails'
 
 const Content = styled.div`
   p {
@@ -34,6 +41,30 @@ const Content = styled.div`
   }
 `
 
+const SyledAuthor = styled.div`
+  display: flex;
+  color: ${WHITE};
+  img {
+    margin-right: 18px;
+  }
+  a {
+    margin-top: 20px;
+    display:block;
+  } 
+`
+
+const Author = ({ name = 'Richard Moss', path = RICHARD_MOSS_PATH, imgSrc = RICHARD }) => (
+  <SyledAuthor>
+    <Image src={imgSrc} circle width={100} />
+    <P>
+      <Link to={`/about-us#${path}`}>{name}</Link>
+      <Span>
+        Oct 4
+    </Span>
+    </P>
+  </SyledAuthor>
+)
+
 const BlogPost = ({ data }) => {
   const { title, subtitle } = data.markdownRemark.frontmatter
   const { html } = data.markdownRemark
@@ -52,9 +83,8 @@ const BlogPost = ({ data }) => {
         fullHeight={false}
         paddingBottom={80}
       >
-        BlogPostedBy
+        <Author />
       </Header>
-
       <Grid>
         <Row>
           <Col md={6} mdOffset={1}>
@@ -75,11 +105,11 @@ const BlogPost = ({ data }) => {
 
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        subtitle
-      }
+        markdownRemark(fields: {slug: {eq: $slug } }) {
+        frontmatter {
+      title
+      subtitle
+    }
       fields {
         slug
       }
