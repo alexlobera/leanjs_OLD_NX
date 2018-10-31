078d99edd4d9fb18e466c3b7da03ded4fc91307c
Richard Moss <richard@englishtutorslive.com>
2018-10-31 19:31:41 +0000

    Remove duplicate content

--- src/components/form/Contact.js
+++ src/components/form/Contact.js
@@ -131,23 +131,6 @@ class ContactForm extends Component {
             </P>
           </React.Fragment>
         }
-        <H3 id="ContactUs">Contact us</H3>
-        <P>
-          The best way to contact us is by emailing us at{' '}
-          <Link to="mailto:hello@reactjsacademy.com">
-            hello@reactjs.academy
-          </Link>
-          .{' '}
-        </P>
-        <P>
-          Otherwise, you can contact us socially on{' '}
-          <Link to="https://twitter.com/reactjsacademy">Twitter</Link>,{' '}
-          <Link to="https://www.instagram.com/reactjsacademy/">Instagram</Link>{' '}
-          and{' '}
-          <Link to="https://www.facebook.com/reactjsacademy/">Facebook</Link> or
-          visit our <Link to="/about-us">About Us page</Link> and directly
-          contact one of our coaches.{' '}
-        </P>        
         <H3>Newsletter & pre-training resources</H3>
         <P>
           Enter your email below and we'll email you with our latest training
