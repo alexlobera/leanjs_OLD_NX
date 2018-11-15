f362d1b96571c38e045876157fe840de58c2c52b
Richard Moss <richard@englishtutorslive.com>
2018-11-15 15:59:49 +0000

    remove console log

--- src/components/payment/checkout/CheckoutContainer.js
+++ src/components/payment/checkout/CheckoutContainer.js
@@ -136,7 +136,6 @@ export class CheckoutContainer extends React.Component {
           },
         })
           .then(({ data }) => {
-            console.log('data', data)
             if (!data.errors) {
               this.props.history.push('/payment-confirmation', {
                 email,
