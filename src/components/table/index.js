e671ec8cf694f057ffbd9f9ccce4d703fa813aac
Richard Moss <richard@englishtutorslive.com>
2018-12-19 13:46:49 +0000

    Create abstract component for table

---
+++ src/components/table/index.js
@@ -0,0 +1,7 @@
+import { Table as responsiveTable, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+import styled from 'styled-components'
+
+const Table = styled(responsiveTable)
+
+export { Table, Thead, Tbody, Tr, Th, Td }
