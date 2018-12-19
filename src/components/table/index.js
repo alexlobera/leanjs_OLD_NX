8b7cf1c1b5de37e0846a09ff96949653c9557041
Richard Moss <richard@englishtutorslive.com>
2018-12-19 13:54:18 +0000

    mend

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,7 +1,12 @@
-import { Table as responsiveTable, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import * as responsiveTable from 'react-super-responsive-table'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
-import styled from 'styled-components'
 
-const Table = styled(responsiveTable)
+const Table = responsiveTable.Table
+const Thead = responsiveTable.Thead
+const Tbody = responsiveTable.Tbody
+const Tr = responsiveTable.Tr
+const Th = responsiveTable.Th
+const Td = responsiveTable.Td
+
 
 export { Table, Thead, Tbody, Tr, Th, Td }
