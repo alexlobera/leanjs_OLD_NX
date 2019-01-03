9feca49eb66b8245c7326775f542d9539a8a055d
Richard Moss <richard@englishtutorslive.com>
2019-01-03 21:36:27 +0000

    Add node-fetch

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions')
+const fetch = require('node-fetch')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
+  response.status(200).send('Hello from Firebase!')
 })
