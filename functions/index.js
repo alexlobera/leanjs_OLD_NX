7ad05f1fdbc8f86475b9a733c571ae3cf7161c5c
Richard Moss <richard@englishtutorslive.com>
2019-01-03 22:43:22 +0000

    Updates

--- functions/index.js
+++ functions/index.js
@@ -4,28 +4,18 @@ const fetch = require('node-fetch')
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  resonse.set('Access-Control-Allow-Origin', '*')
-  if (req.method === 'OPTIONS') {
-    // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET')
-    res.set('Access-Control-Allow-Headers', 'Content-Type')
-    res.set('Access-Control-Max-Age', '3600')
-    res.status(204).send('')
-  } else {
-    // Set CORS headers for the main request
-    res.set('Access-Control-Allow-Origin', '*')
-    res.send('Hello World!')
+  response.set('Access-Control-Allow-Origin', '*')
+  response.send('it worked')
 
-    const AUTOPILOT_UNSUBSCRIBE_TRIGGER_ID = '5a1f31869009404289bc9d63a2b4cca5'
-    const email = request.body.email
-    fetch(
-      `https://api2.autopilothq.com/v1/trigger/${AUTOPILOT_UNSUBSCRIBE_TRIGGER_ID}/contact/${email}`,
-      {
-        method: 'POST',
-        headers: {
-          autopilotapikey: AUTOPILOT_API_KEY,
-        },
-      }
-    )
-  }
+  const AUTOPILOT_UNSUBSCRIBE_TRIGGER_ID = '5a1f31869009404289bc9d63a2b4cca5'
+  const email = request.body.email
+  fetch(
+    `https://api2.autopilothq.com/v1/trigger/${AUTOPILOT_UNSUBSCRIBE_TRIGGER_ID}/contact/${email}`,
+    {
+      method: 'POST',
+      headers: {
+        autopilotapikey: AUTOPILOT_API_KEY,
+      },
+    }
+  )
 })
