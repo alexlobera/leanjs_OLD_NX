28adb9802148a2ee329f475b2c7c42a091cf4e98
Paul Woodley <paul@leanjs.com>
2019-01-09 16:04:23 +0000

    landing pages changed structure and content

--- src/templates/landing.js
+++ src/templates/landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 import Section, { TopSection } from '../components/layout/Section'
 import Grid, { Col, Row } from '../components/layout/Grid'
 import { H2, P } from '../components/text'
@@ -8,6 +9,25 @@ import Header from '../components/layout/Header'
 import { TrustedByLogoList } from '../components/training/TrustedBySection'
 import { UpcomingTrainingSection } from '../components/training'
 import { Breadcrumb } from '../components/navigation'
+import {
+  NotBegginersIcon,
+  RunFastIcon,
+  TargetIcon,
+  TickBadgeIcon,
+  BulletIcon,
+  PeopleNetWorkIcon,
+} from '../components/icons'
+import { Link } from '../components/navigation'
+import Ul, { Li } from '../components/layout/Ul'
+import { LinkButton } from '../components/buttons'
+import {
+  selectTrainings,
+  selectFirstTraining,
+  REACT_BOOTCAMP,
+} from '../config/data'
+import Newsletter from '../components/elements/Newsletter'
+
+const nextTraining = selectFirstTraining(REACT_BOOTCAMP)
 
 const Landing = ({ data }) => {
   const {
@@ -22,43 +42,98 @@ const Landing = ({ data }) => {
       <Breadcrumb
         path={[
           { to: '/', label: 'Home' },
-          { to: '/react-redux-graphql-bootcamp', label: 'React bootcamp' },
-          { to: `${url}`, label: `${country}` },
+          { to: '/react-redux-graphql-training/', label: 'List of trainings' },
+          { to: `${url}`, label: `${city}` },
         ]}
       />
       <Header
-        titleLines={['React Redux GraphQL Bootcamp', `Training in ${city}`]}
-        subtitle={`Take your dev career to the next level in ${city} - ${country} - by mastering<br />React, Redux, and GraphQL - in just 1 week!`}
+        titleLines={['React Redux GraphQL', `training in ${city}`]}
+        subtitle={`Take your dev career to the next level in ${city} - ${country} - by mastering<br />React, Redux, and GraphQL`}
         bgImg="training-event"
       />
       <TopSection>
+        <Section xsBgDark>
+          <Grid>
+            <Card bg="dark">
+              <Row>
+                <Col xs={12} md={6} lg={4} lgOffset={1}>
+                  <Video youtubeID={youtubeID} />
+                </Col>
+                <Col xs={12} md={6} lg={5} lgOffset={1}>
+                  <H2>
+                    React Redux GraphQL in {city}, {country}.
+                  </H2>
+                  {paragraphs.map(paragraph => (
+                    <P>
+                      <div dangerouslySetInnerHTML={{ __html: paragraph }} />
+                    </P>
+                  ))}
+                  <Newsletter />
+                </Col>
+              </Row>
+            </Card>
+          </Grid>
+        </Section>
+      </TopSection>
+      <Section>
         <Grid>
-          <Card border="shadow">
+          <Row>
+            <Col md={5}>
+              <Video youtubeID="6hmKu1-vW-8" />
+              <P>
+                Listen to Polina Stoyanova, a software engineer from tray.io who
+                attended our last bootcamp, on her experience at the bootcamp.
+              </P>
+              <Link to="https://www.youtube.com/channel/UC8eG6zOgWqeIZlJ8KRgEbSQ/videos">
+                Watch further testimonials
+              </Link>
+            </Col>
+            <Col md={5} mdOffset={1}>
+              <H2>Is this React training right for me?</H2>
+              <Ul unstyled>
+                <Li>
+                  <BulletIcon icon={RunFastIcon} />
+                  Extremely rapid, intense learning
+                </Li>
+                <Li>
+                  <BulletIcon icon={NotBegginersIcon} />
+                  Ideal for experienced programmers familiar with good
+                  practices. Not for beginners!
+                </Li>
+                <Li>
+                  <BulletIcon icon={TickBadgeIcon} />
+                  Small classes with expert developer coaches - roughly one for
+                  every four students
+                </Li>
+                <Li>
+                  <BulletIcon icon={TargetIcon} />
+                  Hands-on project-based training - most of the time you'll be
+                  coding.
+                </Li>
+                <Li>
+                  <BulletIcon icon={PeopleNetWorkIcon} />
+                  Join a growing network of alumni for advice, knowledge and
+                  social fun!
+                </Li>
+              </Ul>
+              <P>
+                <LinkButton cta to={nextTraining.pathUrl}>
+                  Next bootcamp:{' '}
+                  {moment(nextTraining.dateStartsOn).format('D MMM')},{' '}
+                  {nextTraining.city}
+                </LinkButton>
+              </P>
+            </Col>
+          </Row>
+        </Grid>
+      </Section>
+      <Section>
+        <Grid>
+          <Card>
             <Row>
-              <Col xs={12} lg={10} lgOffset={1}>
+              <Col xs={12} lg={12}>
                 <H2>Trusted by industry leaders</H2>
                 <TrustedByLogoList />
-              </Col>
-            </Row>
-          </Card>
-        </Grid>
-      </TopSection>
-      <Section xsBgDark>
-        <Grid>
-          <Card bg="dark">
-            <Row>
-              <Col xs={12} md={6} lg={4} lgOffset={1}>
-                <Video youtubeID={youtubeID} />
-              </Col>
-              <Col xs={12} md={6} lg={5} lgOffset={1}>
-                <H2>
-                  React Redux GraphQL in {city}, {country}.
-                </H2>
-                {paragraphs.map(paragraph => (
-                  <P>
-                    <div dangerouslySetInnerHTML={{ __html: paragraph }} />
-                  </P>
-                ))}
               </Col>
             </Row>
           </Card>
@@ -67,7 +142,7 @@ const Landing = ({ data }) => {
       <Section>
         <Grid>
           <Card white border="shadow">
-            <CurriculumBootcamp />
+            <CurriculumBootcamp listTwoCol />
           </Card>
         </Grid>
       </Section>
