1216e91e484239604f6183d2e5b2ad3027be55ea
Will Voelcker <wvoelcker@yahoo.co.uk>
2019-01-11 16:48:33 +0000

    Allow configuring whether or not a particular lazyloading component should lazyLoadByDefault

--- src/components/elements/withLazyLoad.js
+++ src/components/elements/withLazyLoad.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import LazyLoad from 'react-lazyload';
 
-const withLazyLoad = (defaultConfig={}) => Component => ({ lazyLoad=true, ...rest }) => {
+const withLazyLoad = ({defaultConfig={}, lazyLoadByDefault=true} = {}) => Component => ({ lazyLoad=lazyLoadByDefault, ...rest }) => {
 	const element = <Component {...rest} />
-	const lazyLoadConfig = lazyLoad === true ? {} : {...defaultConfig, ...lazyLoad}
+	const lazyLoadConfig = lazyLoad === true ? defaultConfig : {...defaultConfig, ...lazyLoad}
 	return lazyLoad ? <LazyLoad {...lazyLoadConfig}>{element}</LazyLoad> : element
 }
 
