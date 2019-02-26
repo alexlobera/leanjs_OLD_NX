fabbcf076185eeb28a4b573b4f99349c4f87f7d3
Richard Moss <richard@englishtutorslive.com>
2019-02-26 14:53:15 -0800

    revert margin-top to original

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -33,7 +33,7 @@ Section.displayName = 'Section'
 export const TopSection = styled(Section)`
   position: relative;
   @media (min-width: ${SCREEN_SM_MIN}) {
-    margin-top: ${({ marginTop }) => (marginTop ? marginTop : '-60')}px;
+    margin-top: ${({ marginTop }) => (marginTop ? marginTop : '-128')}px;
   }
 `
 
