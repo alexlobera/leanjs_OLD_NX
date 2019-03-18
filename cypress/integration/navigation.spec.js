d862ffb72f4ccd66ae004248fa918ab64d537afd
Richard Moss <richard@englishtutorslive.com>
2019-03-18 13:12:16 +0000

    Update e2e test for text change

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,8 +1,8 @@
-describe('Navigation', function () {
-  it("clicking '1-week React bootcamp' navigates to a correct url", function () {
+describe('Navigation', function() {
+  it("clicking '1-week React bootcamp' navigates to a correct url", function() {
     cy.visit('/')
 
-    cy.contains('1-week React bootcamp >>').click()
+    cy.contains('1-week bootcamp >>').click()
 
     cy.url().should('include', '/react-redux-graphql-bootcamp/')
   })
