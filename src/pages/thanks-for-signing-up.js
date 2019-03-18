7accd27e19eba9c9863e9270e73113475feda3c6
Paul Woodley <paul@leanjs.com>
2019-03-18 16:25:24 +0000

    copy changes to newsletter thanks page

--- src/pages/thanks-for-signing-up.js
+++ src/pages/thanks-for-signing-up.js
@@ -11,8 +11,8 @@ class ThanksForSignUp extends React.Component {
     return (
       <React.Fragment>
         <Header
-          titleLines={['Thanks!']}
-          subtitle="We can’t wait to help you on your React journey."
+          titleLines={['Thanks - your free resources are on their way!']}
+          subtitle="Every week for the next 5 weeks, you'll get a new email with juicy React goodness... You don't have to do a thing."
           bgImg="training-event"
         />
         <TopSection marginTop="-250">
@@ -22,10 +22,14 @@ class ThanksForSignUp extends React.Component {
                 <Col md={5} mdOffset={1}>
                   <H2>What happens now?</H2>
                   <P>
-                    You should shortly recieve an order confirmation and receipt
-                    and further details about the training you’ve signed up to.
-                    Just check your inbox soon (be sure to check your spam
-                    folder if you can’t see it).
+                    Now that you're part of the club, you should be recieving
+                    your first learning resource very soon. Just keep and eye on
+                    your email inbox.
+                  </P>
+                  <P>
+                    If you've made a mistake and you don't want to recieve our
+                    emails (#sadface), just visit our{' '}
+                    <Link to="/unsubscribe/">unsubscribe</Link> page.
                   </P>
                   <P>
                     If you have any questions, please don’t hesitate in
@@ -33,7 +37,8 @@ class ThanksForSignUp extends React.Component {
                     <Link to="mailto:hello@reactjs.academy">
                       hello@reactjs.academy
                     </Link>{' '}
-                    or you can contact us on social media.
+                    or you can shout us on{' '}
+                    <Link to="/community/">social media.</Link>
                   </P>
                 </Col>
                 <Col md={4} mdOffset={1} />
