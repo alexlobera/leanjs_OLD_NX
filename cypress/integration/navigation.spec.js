1eeb4edfc99b0c9c0e4241228c60d25fb98ff214
alexlbr <alex.lobera@gmail.com>
2019-04-18 17:41:31 +0100

    updates cypress test home page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,5 @@
 describe('Navigation', function() {
-  it("clicking '1-week React bootcamp' navigates to a correct url", function() {
+  it("clicking '1-week bootcamp' navigates to a correct url", function() {
     cy.visit('/')
 
     cy.contains('1-week bootcamp >>').click()
