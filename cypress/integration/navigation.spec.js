7967677d42f9e9f0cadf1e6ea8e20c55cf464bb1
Richard Moss <richard@englishtutorslive.com>
2019-04-18 18:24:47 +0100

    Update e2e

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,7 +2,7 @@ describe('Navigation', function() {
   it("clicking '1-week bootcamp' navigates to a correct url", function() {
     cy.visit('/')
 
-    cy.contains('1-week bootcamp >>').click()
+    cy.contains('1-week bootcamp ').click()
 
     cy.url().should('include', '/react-redux-graphql-bootcamp/')
   })
