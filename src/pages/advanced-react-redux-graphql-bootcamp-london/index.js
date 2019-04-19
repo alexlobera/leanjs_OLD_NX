c1a0d4c64cff2738b2bf92f1b0f952c63bc4f32c
Richard Moss <richard@englishtutorslive.com>
2019-04-19 16:29:09 +0100

    Data integration for London advanced trainings

--- src/pages/advanced-react-redux-graphql-bootcamp-london/index.js
+++ src/pages/advanced-react-redux-graphql-bootcamp-london/index.js
@@ -11,6 +11,8 @@ import { CurriculumAdvancedReact } from '../../components/curriculum'
 import {
   UpcomingTrainingSection,
   withUpcomingTrainings,
+  selectUpcomingTrainings,
+  selectNthTraining,
   AttendeeQuote,
   TrainingDetails,
   ALEX_LOBERA,
@@ -28,154 +30,150 @@ import { Image } from '../../components/elements'
 import header from '../../components/layout/Header.json'
 import { PaymentSection } from '../../components/payment'
 import { Link, Breadcrumb } from '../../components/navigation'
-import { selectFirstTraining, ADVANCED_REACT, LONDON } from '../../config/data'
+import { ADVANCED_REACT, LONDON } from '../../config/data'
 import { LIST_TWO_COL } from '../../components/curriculum/selectCurriculumLayout'
 
-const training = selectFirstTraining(ADVANCED_REACT, LONDON)
-
-const BootcampLondon = ({ trainings }) => (
-  <React.Fragment>
-    <Breadcrumb
-      path={[
-        { to: '/', label: 'Home' },
-        {
-          to: '/advanced-react-redux-graphql-bootcamp',
-          label: 'Advanced React',
-        },
-        {
-          to: '/advanced-react-redux-graphql-bootcamp-london',
-          label: 'London',
-        },
-      ]}
-    />
-    <Header
-      titleLines={['Advanced React Redux GraphQL training - London']}
-      subtitle="Take your React career to the next level by mastering advanced React, Redux, and GraphQL - in just 3 days!"
-      links={header.landingTraining.links}
-      bgImg="training-event"
-      type={ADVANCED_REACT}
-      training={training}
-    />
-    <TopSection xsBgDark top>
-      <Grid>
-        <Card bg="dark">
+const BootcampLondon = ({ trainings, trainingLoading, trainingError }) => {
+  const upcomingAdvancedTrainings = selectUpcomingTrainings({
+    trainings,
+    type: ADVANCED_REACT,
+    city: LONDON,
+  })
+  const training =
+    selectNthTraining({ trainings: upcomingAdvancedTrainings }) || {}
+  return (
+    <React.Fragment>
+      <Breadcrumb
+        path={[
+          { to: '/', label: 'Home' },
+          {
+            to: '/advanced-react-redux-graphql-bootcamp',
+            label: 'Advanced React',
+          },
+          {
+            to: '/advanced-react-redux-graphql-bootcamp-london',
+            label: 'London',
+          },
+        ]}
+      />
+      <Header
+        titleLines={['Advanced React Redux GraphQL training - London']}
+        subtitle="Take your React career to the next level by mastering advanced React, Redux, and GraphQL - in just 3 days!"
+        links={header.landingTraining.links}
+        bgImg="training-event"
+        type={ADVANCED_REACT}
+        training={training}
+        showInfoBox={true}
+      />
+      <TopSection xsBgDark top>
+        <Grid>
+          <Card bg="dark">
+            <Row>
+              <Col xs={12} md={6} lg={5} lgOffset={1}>
+                <PaymentSection
+                  training={training}
+                  trainingLoading={trainingLoading}
+                  trainingError={trainingError}
+                />
+              </Col>
+              <Col xs={12} md={6} lg={4} lgOffset={1}>
+                <Video youtubeId="yvROXLQ1jHg" />
+                <TrainingDetails
+                  foodIncluded={false}
+                  coaches={[
+                    ALEX_LOBERA,
+                    HORACIO_HERRERA,
+                    RICHARD_MOSS,
+                    WILL_VOELCKER,
+                  ]}
+                />
+              </Col>
+            </Row>
+          </Card>
+        </Grid>
+      </TopSection>
+      <Section>
+        <Grid>
           <Row>
-            <Col xs={12} md={6} lg={5} lgOffset={1}>
-              <PaymentSection
-                data={{
-                  trainingInstanceId: training.trainingInstanceId,
-                  price: training.price,
-                  currency: training.currency,
-                }}
-              />
-            </Col>
-            <Col xs={12} md={6} lg={4} lgOffset={1}>
-              <Video youtubeId="yvROXLQ1jHg" />
-              <TrainingDetails
-                date={training.dates}
-                timing="9:00am - 6:30pm"
-                foodIncluded={false}
-                location={
-                  <React.Fragment>
-                    {training.location}.{' '}
-                    <Link to="https://goo.gl/maps/jjX9zs5Ags32">
-                      See on map
-                    </Link>
-                  </React.Fragment>
-                }
-                coaches={[
-                  ALEX_LOBERA,
-                  HORACIO_HERRERA,
-                  RICHARD_MOSS,
-                  WILL_VOELCKER,
-                ]}
-              />
+            <HideComponentsUsingCss xs sm>
+              <Col md={6} lg={5}>
+                <Image
+                  src={BOOTCAMP_COLLAB}
+                  width="100%"
+                  alt="ReactJS Academy coach Tiago assisting two students, inspecting their laptop screens and ready to answer their questions"
+                />
+              </Col>
+            </HideComponentsUsingCss>
+            <Col md={6} lg={5} lgOffset={1}>
+              <H2Ref>
+                Is this 1-day Advanced React training right for me? Are you...{' '}
+                <Link to="#target-audience" name="target-audience">
+                  #
+                </Link>
+              </H2Ref>
+              <Ul unstyled>
+                <Li>
+                  <BulletIcon icon={NotBegginerIcon} />A{' '}
+                  <strong>React developer with 1+ year of development</strong>{' '}
+                  under your belt using React?
+                </Li>
+                <Li>
+                  <BulletIcon icon={ReactIcon} />
+                  Taking a step forward to become a{' '}
+                  <strong>Senior React developer</strong> able to make critical
+                  decisions about the architecture of a React application.
+                </Li>
+                <Li>
+                  <BulletIcon icon={CollabsIcon} />
+                  Not satisfied with the pace of online learning and it's lack
+                  of 1-on-1 mentoring?
+                </Li>
+              </Ul>
+              <P>
+                If you've said 'yes' to these, our{' '}
+                <strong>1-day advanced React training</strong> could be for you!
+              </P>
+              <H3>Not for beginner devs!</H3>
+              <P>
+                This is a bootcamp for React developers that are experienced
+                with React. If you don't have 1+ year of experience using React
+                we recommend you to attend our{' '}
+                <Link to="/react-redux-graphql-bootcamp">React Bootcamp</Link>.
+              </P>
             </Col>
           </Row>
-        </Card>
-      </Grid>
-    </TopSection>
-    <Section>
-      <Grid>
-        <Row>
-          <HideComponentsUsingCss xs sm>
-            <Col md={6} lg={5}>
-              <Image
-                src={BOOTCAMP_COLLAB}
-                width="100%"
-                alt="ReactJS Academy coach Tiago assisting two students, inspecting their laptop screens and ready to answer their questions"
-              />
-            </Col>
-          </HideComponentsUsingCss>
-          <Col md={6} lg={5} lgOffset={1}>
-            <H2Ref>
-              Is this 1-day Advanced React training right for me? Are you...{' '}
-              <Link to="#target-audience" name="target-audience">
-                #
-              </Link>
-            </H2Ref>
-            <Ul unstyled>
-              <Li>
-                <BulletIcon icon={NotBegginerIcon} />A{' '}
-                <strong>React developer with 1+ year of development</strong>{' '}
-                under your belt using React?
-              </Li>
-              <Li>
-                <BulletIcon icon={ReactIcon} />
-                Taking a step forward to become a{' '}
-                <strong>Senior React developer</strong> able to make critical
-                decisions about the architecture of a React application.
-              </Li>
-              <Li>
-                <BulletIcon icon={CollabsIcon} />
-                Not satisfied with the pace of online learning and it's lack of
-                1-on-1 mentoring?
-              </Li>
-            </Ul>
-            <P>
-              If you've said 'yes' to these, our{' '}
-              <strong>1-day advanced React training</strong> could be for you!
-            </P>
-            <H3>Not for beginner devs!</H3>
-            <P>
-              This is a bootcamp for React developers that are experienced with
-              React. If you don't have 1+ year of experience using React we
-              recommend you to attend our{' '}
-              <Link to="/react-redux-graphql-bootcamp">React Bootcamp</Link>.
-            </P>
-          </Col>
-        </Row>
-      </Grid>
-    </Section>
-    <Section>
-      <Grid>
-        <Row>
-          <Col lg={10} lgOffset={1}>
-            <AttendeeQuote
-              quote="Technology nowadays changes very often and in future you may not be able to find a job with the things you know - you have to keep up. I like the fact that we got to write code rather than focus on theory."
-              fullname="Catalin Cislariu"
-              job="Senior Developer"
-              company="KLEIDO LTD"
-              profilePicUrl={CATALIN}
-            />
-          </Col>
-        </Row>
-      </Grid>
-    </Section>
-
-    <Section>
-      <Grid>
-        <Card border="shadow">
+        </Grid>
+      </Section>
+      <Section>
+        <Grid>
           <Row>
             <Col lg={10} lgOffset={1}>
-              <CurriculumAdvancedReact layout={LIST_TWO_COL} />
+              <AttendeeQuote
+                quote="Technology nowadays changes very often and in future you may not be able to find a job with the things you know - you have to keep up. I like the fact that we got to write code rather than focus on theory."
+                fullname="Catalin Cislariu"
+                job="Senior Developer"
+                company="KLEIDO LTD"
+                profilePicUrl={CATALIN}
+              />
             </Col>
           </Row>
-        </Card>
-      </Grid>
-    </Section>
-    <UpcomingTrainingSection trainings={trainings} />
-  </React.Fragment>
-)
+        </Grid>
+      </Section>
+
+      <Section>
+        <Grid>
+          <Card border="shadow">
+            <Row>
+              <Col lg={10} lgOffset={1}>
+                <CurriculumAdvancedReact layout={LIST_TWO_COL} />
+              </Col>
+            </Row>
+          </Card>
+        </Grid>
+      </Section>
+      <UpcomingTrainingSection trainings={trainings} />
+    </React.Fragment>
+  )
+}
 
 export default withUpcomingTrainings()(BootcampLondon)
