b5cdda419c223bcfba23098c8284aaeae4caf2c6
Richard Moss <richard@englishtutorslive.com>
2019-04-22 16:45:35 +0100

    Make data integration updates to native instance to pass e2e tests

--- src/pages/react-native-bootcamp-london.js
+++ src/pages/react-native-bootcamp-london.js
@@ -12,6 +12,8 @@ import { BOOTCAMP_COLLAB, CATALIN } from '../config/images'
 import {
   UpcomingTrainingSection,
   withUpcomingTrainings,
+  selectNthTraining,
+  selectUpcomingTrainings,
   AttendeeQuote,
   TrainingDetails,
   HORACIO_HERRERA,
@@ -29,134 +31,132 @@ import { PaymentSection } from '../components/payment'
 import { REACT_NATIVE, LONDON } from '../config/data'
 import { LIST_TWO_COL } from '../components/curriculum/selectCurriculumLayout'
 
-const ReactNativeBoocampLondon = ({ trainings }) => (
-  <React.Fragment>
-    <Breadcrumb
-      path={[
-        { to: '/', label: 'Home' },
-        { to: '/react-native-bootcamp', label: 'React Native' },
-        { to: '/react-native-bootcamp-london', label: 'London' },
-      ]}
-    />
-    <Header
-      titleLines={[`React Native Training - London`]}
-      subtitle="Take your React developer career to the next level by<br />learning React Native, in only one day. "
-      links={header.landingTraining.links}
-      bgImg="training-event"
-      type={REACT_NATIVE}
-      training={training}
-    />
-    <TopSection xsBgDark top>
-      <Grid>
-        <Card bg="dark">
+const ReactNativeBoocampLondon = ({ trainings }) => {
+  const upcomingNativeTrainings = selectUpcomingTrainings({
+    trainings,
+    type: REACT_NATIVE,
+    city: LONDON,
+  })
+  const training =
+    selectNthTraining({ trainings: upcomingNativeTrainings }) || {}
+  return (
+    <React.Fragment>
+      <Breadcrumb
+        path={[
+          { to: '/', label: 'Home' },
+          { to: '/react-native-bootcamp', label: 'React Native' },
+          { to: '/react-native-bootcamp-london', label: 'London' },
+        ]}
+      />
+      <Header
+        titleLines={[`React Native Training - London`]}
+        subtitle="Take your React developer career to the next level by<br />learning React Native, in only one day. "
+        links={header.landingTraining.links}
+        bgImg="training-event"
+        type={REACT_NATIVE}
+        training={training}
+        showInfoBox={true}
+      />
+      <TopSection xsBgDark top>
+        <Grid>
+          <Card bg="dark">
+            <Row>
+              <Col xs={12} md={6} lg={5} lgOffset={1}>
+                <PaymentSection
+                  training={training}
+                  trainingError={trainingError}
+                  trainingLoading={trainingLoading}
+                />
+              </Col>
+              <Col xs={12} md={6} lg={4} lgOffset={1}>
+                <Video youtubeId="yvROXLQ1jHg" />
+                <TrainingDetails
+                  foodIncluded
+                  coaches={[HORACIO_HERRERA, ALEX_LOBERA]}
+                />
+              </Col>
+            </Row>
+          </Card>
+        </Grid>
+      </TopSection>
+      <Section>
+        <Grid>
           <Row>
-            <Col xs={12} md={6} lg={5} lgOffset={1}>
-              <PaymentSection
-                data={{
-                  trainingInstanceId: training.trainingInstanceId,
-                  price: training.price,
-                  discountPrice: training.discountPrice,
-                  priceGoesUpOn: training.priceGoesUpOn,
-                  currency: training.currency,
-                }}
-              />
-            </Col>
-            <Col xs={12} md={6} lg={4} lgOffset={1}>
-              <Video youtubeId="yvROXLQ1jHg" />
-              <TrainingDetails
-                date={training.dates}
-                timing="9am - 6:30pm"
-                location={
-                  <React.Fragment>
-                    {training.location}.{' '}
-                    <Link to="https://www.google.com/maps/place/Eden+House,+8+Spital+Square,+London+E1+6DQ/@51.5207898,-0.0778216,17z/data=!3m1!4b1!4m5!3m4!1s0x48761cb166ce716f:0x7309676fc6ea9b2b!8m2!3d51.5207898!4d-0.0778216">
-                      See on map
-                    </Link>
-                  </React.Fragment>
-                }
-                foodIncluded
-                coaches={[HORACIO_HERRERA, ALEX_LOBERA]}
-              />
+            <HideComponentsUsingCss xs sm>
+              <Col md={6} lg={5}>
+                <Image src={BOOTCAMP_COLLAB} width="100%" />
+              </Col>
+            </HideComponentsUsingCss>
+            <Col md={6} lg={5} lgOffset={1}>
+              <H2Ref>
+                Is this React Native training right for me? Are you...{' '}
+                <Link to="#target-audience" name="target-audience">
+                  #
+                </Link>
+              </H2Ref>
+              <Ul unstyled>
+                <Li>
+                  <BulletIcon icon={NotBegginersIcon} />
+                  You have at least a few months of experience using React on
+                  the web. If you don't know React, we recommend you first to
+                  attend our{' '}
+                  <Link to="/react-redux-graphql-bootcamp">React Bootcamp</Link>
+                </Li>
+                <Li>
+                  <BulletIcon icon={ReactIcon} />
+                  Taking a step forward to become a React Native Specialist able
+                  to make critical decisions about the architecture of a React
+                  Native application.
+                </Li>
+                <Li>
+                  <BulletIcon icon={CollabsIcon} />
+                  Not satisfied with the pace of online learning and it's lack
+                  of 1-on-1 mentoring?
+                </Li>
+              </Ul>
+              <P>
+                If you've said 'yes' to these, our training could be for you!
+              </P>
+              <H3>Not for beginner devs!</H3>
+              <P>
+                This is not a training for React beginners. If you don't know
+                React, we recommend you first to attend our{' '}
+                <Link to="/react-redux-graphql-bootcamp">React Bootcamp</Link>.
+              </P>
             </Col>
           </Row>
-        </Card>
-      </Grid>
-    </TopSection>
-    <Section>
-      <Grid>
-        <Row>
-          <HideComponentsUsingCss xs sm>
-            <Col md={6} lg={5}>
-              <Image src={BOOTCAMP_COLLAB} width="100%" />
-            </Col>
-          </HideComponentsUsingCss>
-          <Col md={6} lg={5} lgOffset={1}>
-            <H2Ref>
-              Is this React Native training right for me? Are you...{' '}
-              <Link to="#target-audience" name="target-audience">
-                #
-              </Link>
-            </H2Ref>
-            <Ul unstyled>
-              <Li>
-                <BulletIcon icon={NotBegginersIcon} />
-                You have at least a few months of experience using React on the
-                web. If you don't know React, we recommend you first to attend
-                our{' '}
-                <Link to="/react-redux-graphql-bootcamp">React Bootcamp</Link>
-              </Li>
-              <Li>
-                <BulletIcon icon={ReactIcon} />
-                Taking a step forward to become a React Native Specialist able
-                to make critical decisions about the architecture of a React
-                Native application.
-              </Li>
-              <Li>
-                <BulletIcon icon={CollabsIcon} />
-                Not satisfied with the pace of online learning and it's lack of
-                1-on-1 mentoring?
-              </Li>
-            </Ul>
-            <P>If you've said 'yes' to these, our training could be for you!</P>
-            <H3>Not for beginner devs!</H3>
-            <P>
-              This is not a training for React beginners. If you don't know
-              React, we recommend you first to attend our{' '}
-              <Link to="/react-redux-graphql-bootcamp">React Bootcamp</Link>.
-            </P>
-          </Col>
-        </Row>
-      </Grid>
-    </Section>
-    <Section>
-      <Grid>
-        <Row>
-          <Col lg={10} lgOffset={1}>
-            <AttendeeQuote
-              quote="Technology nowadays changes very often and in future you may not be able to find a job with the things you know - you have to keep up. I like the fact that we got to write code rather than focus on theory."
-              fullname="Catalin Cislariu"
-              job="Senior Developer"
-              company="KLEIDO LTD"
-              profilePicUrl={CATALIN}
-            />
-          </Col>
-        </Row>
-      </Grid>
-    </Section>
-
-    <Section>
-      <Grid>
-        <Card border="shadow">
+        </Grid>
+      </Section>
+      <Section>
+        <Grid>
           <Row>
             <Col lg={10} lgOffset={1}>
-              <CurriculumReactNative layout={LIST_TWO_COL} />
+              <AttendeeQuote
+                quote="Technology nowadays changes very often and in future you may not be able to find a job with the things you know - you have to keep up. I like the fact that we got to write code rather than focus on theory."
+                fullname="Catalin Cislariu"
+                job="Senior Developer"
+                company="KLEIDO LTD"
+                profilePicUrl={CATALIN}
+              />
             </Col>
           </Row>
-        </Card>
-      </Grid>
-    </Section>
-    <UpcomingTrainingSection trainings={trainings} />
-  </React.Fragment>
-)
+        </Grid>
+      </Section>
+
+      <Section>
+        <Grid>
+          <Card border="shadow">
+            <Row>
+              <Col lg={10} lgOffset={1}>
+                <CurriculumReactNative layout={LIST_TWO_COL} />
+              </Col>
+            </Row>
+          </Card>
+        </Grid>
+      </Section>
+      <UpcomingTrainingSection trainings={trainings} />
+    </React.Fragment>
+  )
+}
 
 export default withUpcomingTrainings()(ReactNativeBoocampLondon)
