8ed89303a5bde5edfc0cff586c18741f7f48bb4e
Richard Moss <richard@englishtutorslive.com>
2019-04-27 23:33:39 +0200

    Tidy up page

--- src/pages/react-workshops.js
+++ src/pages/react-workshops.js
@@ -1,50 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import Link from '../components/navigation/Link'
 import { LinkButton } from '../components/buttons'
-import Section, { TopSection } from '../components/layout/Section'
+import { TopSection } from '../components/layout/Section'
 import Grid, { Col, Row } from '../components/layout/Grid'
-import { H2, H2Ref, H3, P, H4, H5 } from '../components/text'
+import { H2, P, H4, H5 } from '../components/text'
 import {
   UpcomingTrainingSection,
   withUpcomingTrainings,
 } from '../components/training'
-import Ul, { Li } from '../components/layout/Ul'
 import Header from '../components/layout/Header'
-import CallToActionNextTrainings from '../components/layout/CallToActionNextTrainings'
-import { Card, Video, Image, Newsletter } from '../components/elements'
-import { Blockquote } from '../components/text'
-import {
-  RICHARD,
-  HORACIO,
-  WILL,
-  ADAM,
-  EVA,
-  ROY,
-  DAVID,
-  FRANCISCO,
-  ABOUT_VALUES,
-} from '../config/images'
+import { Card, Newsletter } from '../components/elements'
 import { GREY2 } from '../config/styles'
-import { HideComponentsUsingCss } from '../components/utils'
-import trackUserBehaviour, {
-  SOCIAL_NETWORK_LINK_CLICK,
-} from '../components/utils/trackUserBehaviour'
-import { SCREEN_SM_MAX } from '../components/utils'
-import LeanJSsprints from '../components/elements/LeanJSsprints'
-
-const CoachTitle = styled(H3)`
-  & {
-    padding-top: 0;
-  }
-`
-
-const BecomeACoachWrapper = styled.div`
-  @media (max-width: ${SCREEN_SM_MAX}) {
-    text-align: center;
-    padding-bottom: 18px;
-  }
-`
 
 const WorkshopCard = styled.div`
   border: 1px solid black;
@@ -55,6 +21,7 @@ const WorkshopCard = styled.div`
     padding-left: 1rem;
   }
 `
+const PROVISIONAL_WORKSHOP_PRICE = '£300+VAT'
 
 const Workshops = ({ trainings }) => (
   <React.Fragment>
@@ -73,7 +40,7 @@ const Workshops = ({ trainings }) => (
                 <Col xs={12} sm={6} md={4}>
                   <WorkshopCard>
                     <H4>React Fundamentals & ModernJS</H4>
-                    <H5>£1000+VAT</H5>
+                    <H5>{PROVISIONAL_WORKSHOP_PRICE}</H5>
                     <P>
                       Learn the basics of React and jumpstart your way into a
                       new coding ecosystem
@@ -86,7 +53,7 @@ const Workshops = ({ trainings }) => (
                 <Col xs={12} sm={6} md={4}>
                   <WorkshopCard>
                     <H4>Styling in React and Design Systems</H4>
-                    <H5>£1000+VAT</H5>
+                    <H5>{PROVISIONAL_WORKSHOP_PRICE}</H5>
                     <P>
                       See how React can look gorgeous and encourage design
                       consistency
@@ -99,7 +66,7 @@ const Workshops = ({ trainings }) => (
                 <Col xs={12} sm={6} md={4}>
                   <WorkshopCard>
                     <H4>React Hooks & Suspense</H4>
-                    <H5>£1000+VAT</H5>
+                    <H5>{PROVISIONAL_WORKSHOP_PRICE}</H5>
                     <P>
                       Learn 2 of the newest and most exciting features in React
                     </P>
@@ -112,7 +79,7 @@ const Workshops = ({ trainings }) => (
                 <Col xs={12} sm={6} md={4}>
                   <WorkshopCard>
                     <H4>Performance & FP in React</H4>
-                    <H5>£1000+VAT</H5>
+                    <H5>{PROVISIONAL_WORKSHOP_PRICE}</H5>
                     <P>Discover best practice for permonant React apps</P>
                     <LinkButton variant="secondary" to="/interest-form">
                       Join Waitlist
@@ -122,7 +89,7 @@ const Workshops = ({ trainings }) => (
                 <Col xs={12} sm={6} md={4}>
                   <WorkshopCard>
                     <H4>React with GraphQL client</H4>
-                    <H5>£1000+VAT</H5>
+                    <H5>{PROVISIONAL_WORKSHOP_PRICE}</H5>
                     <P>Consume GraphQL APIs in any React application</P>
                     <LinkButton variant="secondary" to="/interest-form">
                       Join Waitlist
@@ -132,7 +99,7 @@ const Workshops = ({ trainings }) => (
                 <Col xs={12} sm={6} md={4}>
                   <WorkshopCard>
                     <H4>Testing in React</H4>
-                    <H5>£1000+VAT</H5>
+                    <H5>{PROVISIONAL_WORKSHOP_PRICE}</H5>
                     <P>
                       Ensure consistent, reliable code across the React
                       ecosystem
@@ -145,7 +112,7 @@ const Workshops = ({ trainings }) => (
                 <Col xs={12} sm={6} md={4}>
                   <WorkshopCard>
                     <H4>React Native</H4>
-                    <H5>£1000+VAT</H5>
+                    <H5>{PROVISIONAL_WORKSHOP_PRICE}</H5>
                     <P>Learn how to take your React skills into native apps</P>
                     <LinkButton variant="secondary" to="/interest-form">
                       Join Waitlist
