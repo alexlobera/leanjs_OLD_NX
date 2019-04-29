54900b8e1213147c7493881533818b7c70a3c7e9
Richard Moss <richard@englishtutorslive.com>
2019-04-29 17:37:42 +0100

    Tempory styles for checkbox pending update

--- src/components/form/Checkbox.js
+++ src/components/form/Checkbox.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 import FieldInput from './FieldInput'
 import Label from '../text/Label'
+
 const defaultSize = 2
 
 const Icon = styled.svg`
@@ -24,13 +25,14 @@ const HiddenCheckbox = styled(FieldInput).attrs({
   width: 1px;
 `
 const StyledCheckbox = styled.div`
+  margin: 1em 0;
   border: 1px solid black;
-    margin: 1em 0;
+  background: ${props => (props.checked ? 'salmon' : 'papayawhip')}
   height: ${props => (props.size ? props.size : defaultSize)}em;
   width: ${props => (props.size ? props.size : defaultSize)}em;
   ${HiddenCheckbox}:focus + & {
     box-shadow: 0 0 0 3px pink; !important
-  }
+  } // RM: focus needs to be fixed, it works with simple styled input
   ${Icon} {
     visibility: ${props => (props.checked ? 'visible' : 'hidden')};
   }
@@ -40,10 +42,10 @@ const LabelText = styled.span`
   align-self: center;
 `
 
-const Checkbox = ({ className, checked, label, ...props }) => (
+const Checkbox = ({ checked, label, ...props }) => (
   <React.Fragment>
     <HiddenCheckbox checked={checked} {...props} />
-    <StyledCheckbox checked={checked} label={label}>
+    <StyledCheckbox checked={checked} label={label} {...props}>
       <Icon viewBox="0 0 24 24">
         <polyline points="20 6 9 17 4 12" />
       </Icon>
@@ -60,9 +62,10 @@ const CheckBoxContainer = props => {
     <React.Fragment>
       <Label flex>
         <Checkbox
-          {...props}
+          className={props.className}
           checked={checked}
           onChange={handleCheckboxChange}
+          {...props}
         />
         <LabelText>{props.label}</LabelText>
       </Label>
