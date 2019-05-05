da9d6e7c12a470567f8d108a49545118702efa93
Richard Moss <richard@englishtutorslive.com>
2019-05-05 23:30:52 +0100

    Removes unused vars including React

--- src/components/curriculum/CurriculumCard.js
+++ src/components/curriculum/CurriculumCard.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from 'styled-components'
 import { CALLTOACTIONRED, FONT_FAMILY } from '../../config/styles'
 
--- src/components/elements/Alert.js
+++ src/components/elements/Alert.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from 'styled-components'
 import { PINK, FONT_FAMILY } from '../../config/styles'
 
