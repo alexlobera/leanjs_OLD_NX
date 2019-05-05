310a177aa4893d2a1041a880aa3738bdf6bd6ee7
Richard Moss <richard@englishtutorslive.com>
2019-05-05 23:25:01 +0100

    Update name of boundActionCreators as per Gatsby v2

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,8 +6,8 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-  const { createNodeField } = boundActionCreators
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({
       node,
@@ -22,8 +22,8 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   }
 }
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
   return new Promise((resolve, reject) => {
     graphql(`
       {
