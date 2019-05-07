932e170e5be9f0697e926ab3222aeb8f7d14401f
alexlbr <alex.lobera@gmail.com>
2019-05-07 21:52:03 +0100

    removes jquery temporarely to test if autopilot heads up works

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,25 +11,26 @@ exports.onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
     pluginOptions.includeInDevelopment
   ) {
     setHeadComponents([
-      <script
-        key="jquery"
-        dangerouslySetInnerHTML={{
-          __html: stripIndent`
-          ;(function() {
-            function downloadJSAtOnload() {
-              var element = document.createElement('script')
-              element.async = true
-              element.src = 'https://unpkg.com/jquery@3.4.1/dist/jquery.min.js'
-              document.body.appendChild(element)
-            }
-            if (window.addEventListener)
-              window.addEventListener('load', downloadJSAtOnload, false)
-            else if (window.attachEvent) window.attachEvent('onload', downloadJSAtOnload)
-            else window.onload = downloadJSAtOnload
-          })();
-        `,
-        }}
-      />,
+      // [removes it temporarely to test if autopilot heads up works]
+      // <script
+      //   key="jquery"
+      //   dangerouslySetInnerHTML={{
+      //     __html: stripIndent`
+      //     ;(function() {
+      //       function downloadJSAtOnload() {
+      //         var element = document.createElement('script')
+      //         element.async = true
+      //         element.src = 'https://unpkg.com/jquery@3.4.1/dist/jquery.min.js'
+      //         document.body.appendChild(element)
+      //       }
+      //       if (window.addEventListener)
+      //         window.addEventListener('load', downloadJSAtOnload, false)
+      //       else if (window.attachEvent) window.attachEvent('onload', downloadJSAtOnload)
+      //       else window.onload = downloadJSAtOnload
+      //     })();
+      //   `,
+      //   }}
+      // />,
       <script
         key="plugin-autopilot"
         dangerouslySetInnerHTML={{
