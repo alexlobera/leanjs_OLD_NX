d3ccf1895f83bae92ee1acd2e9fe186bb29f1932
Richard Moss <richard@englishtutorslive.com>
2019-05-15 16:59:17 +0100

    move config to . file

--- src/DELETEME/Firebase.js
+++ src/DELETEME/Firebase.js
@@ -1,14 +1,4 @@
-// import { config } from '../../.firebase'
-
-const config = {
-  apiKey: 'AIzaSyCLNdkfgfG1o17sdf4APkxXZts_PDyIN4g',
-  authDomain: 'reactgraphqlacademy.firebaseapp.com',
-  databaseURL: 'https://reactgraphqlacademy.firebaseio.com',
-  projectId: 'reactgraphqlacademy',
-  storageBucket: 'reactgraphqlacademy.appspot.com',
-  messagingSenderId: '589792712908',
-  appId: '1:589792712908:web:3b7616bf6beb36f2',
-}
+import { config } from '../../.firebase'
 
 export class Firebase {
   constructor(app) {
