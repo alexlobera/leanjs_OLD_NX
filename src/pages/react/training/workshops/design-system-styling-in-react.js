211f4670b3a2bd9d9402ca0efacda2aeee1ea2a2
Paul Woodley <paul@leanjs.com>
2019-05-23 10:15:29 +0100

    attendee quote component redone - more work to be done

--- src/pages/react/training/workshops/design-system-styling-in-react.js
+++ src/pages/react/training/workshops/design-system-styling-in-react.js
@@ -159,6 +159,7 @@ const StylingDesignSystemWorkshop = () => (
                     fullname="Catalin Cislariu"
                     job="Senior Developer"
                     company="KLEIDO LTD"
+                    profilePic
                     profilePicUrl={CATALIN}
                   />
                 </Col>
