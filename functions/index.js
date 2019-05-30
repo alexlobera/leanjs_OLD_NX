ad92c5828bf7d3d460f79f376a7ddd099ada081e
alexlbr <alex.lobera@gmail.com>
2019-05-30 17:51:48 +0100

    abstracts post to autopilot in cloud functions

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch')
 const express = require('express')
 
 const api = express()
+const AUTOPILOT_API_KEY = functions.config().autopilot.key
 
 api.use(optionsMiddleware)
 api.post('/unsubscribe', unsubscribe)
@@ -10,6 +11,20 @@ api.post('/sessionSubscribe', sessionSubscribe)
 api.post('/subscribe', subscribe)
 
 exports.api = functions.https.onRequest(api)
+
+async function postToAutopilot(endpoint, jsonBody) {
+  const res = await fetch(`https://api2.autopilothq.com/v1/${endpoint}`, {
+    method: 'POST',
+    headers: {
+      autopilotapikey: AUTOPILOT_API_KEY,
+    },
+    body: jsonBody ? JSON.stringify(jsonBody) : undefined,
+  })
+  await res.json()
+  console.log(json)
+
+  return json
+}
 
 function optionsMiddleware(request, response, next) {
   response.set('Access-Control-Allow-Origin', '*')
@@ -24,31 +39,20 @@ function optionsMiddleware(request, response, next) {
   }
 }
 
-function unsubscribe(request, response) {
+async function unsubscribe(request, response) {
   const AUTOPILOT_UNSUBSCRIBE_TRIGGER_ID = '0008'
   const email = request && request.body && request.body.email
-  const AUTOPILOT_API_KEY = functions.config().autopilot.key
   if (email) {
-    fetch(
-      `https://api2.autopilothq.com/v1/trigger/${AUTOPILOT_UNSUBSCRIBE_TRIGGER_ID}/contact/${email}`,
-      {
-        method: 'POST',
-        headers: {
-          autopilotapikey: AUTOPILOT_API_KEY,
-        },
-      }
+    await postToAutopilot(
+      `/trigger/${AUTOPILOT_UNSUBSCRIBE_TRIGGER_ID}/contact/${email}`
     )
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(console.log('not working'))
-
     response.status(200).send('it worked')
   } else {
     response.status(401).send('no email')
   }
 }
 
-function sessionSubscribe(request, response) {
+async function sessionSubscribe(request, response) {
   const data = request && request.body
   const {
     name,
@@ -62,66 +66,44 @@ function sessionSubscribe(request, response) {
     resources,
     native,
   } = data
-  const AUTOPILOT_API_KEY = functions.config().autopilot.key
   if (email) {
-    fetch(`https://api2.autopilothq.com/v1/contact`, {
-      method: 'POST',
-      headers: {
-        autopilotapikey: AUTOPILOT_API_KEY,
-      },
-      body: JSON.stringify({
-        contact: {
-          FirstName: name,
-          Email: email,
-          LeadSource: '1-day workshops form',
-          _autopilot_list: 'contactlist_37B9CE06-F48D-4F7B-A119-4725B474EF2C',
-          custom: {
-            'boolean--Fundamentals--Session': fundamentals,
-            'boolean--Styling--Session': styling,
-            'boolean--Hooks--Session': hooks,
-            'boolean--Perf--Session': perf,
-            'boolean--GQLclient--Session': gqlclient,
-            'boolean--Testing--Session': testing,
-            'boolean--Resources--Signup': resources,
-            'boolean--Native--Signup': native,
-          },
+    await postToAutopilot(`/contact`, {
+      contact: {
+        FirstName: name,
+        Email: email,
+        LeadSource: '1-day workshops form',
+        _autopilot_list: 'contactlist_37B9CE06-F48D-4F7B-A119-4725B474EF2C',
+        custom: {
+          'boolean--Fundamentals--Session': fundamentals,
+          'boolean--Styling--Session': styling,
+          'boolean--Hooks--Session': hooks,
+          'boolean--Perf--Session': perf,
+          'boolean--GQLclient--Session': gqlclient,
+          'boolean--Testing--Session': testing,
+          'boolean--Resources--Signup': resources,
+          'boolean--Native--Signup': native,
         },
-      }),
+      },
     })
-      .then(res => res.json())
-      .then(json => console.log('json response:', json))
-      .catch(console.log('not working'))
-
     response.status(200).send('it worked')
   } else {
     response.status(401).send('no email')
   }
 }
 
-function subscribe(request, response) {
+async function subscribe(request, response) {
   const email = request && request.body && request.body.email
   const pathname = request && request.body && request.body.pathname
-  const AUTOPILOT_API_KEY = functions.config().autopilot.key
   if (email) {
-    fetch(`https://api2.autopilothq.com/v1/contact`, {
-      method: 'POST',
-      headers: {
-        autopilotapikey: AUTOPILOT_API_KEY,
-      },
-      body: JSON.stringify({
-        contact: {
-          Email: email,
-          LeadSource: 'Footer contact form',
-          custom: {
-            'string--From--Path': pathname,
-          },
+    await postToAutopilot(`/contact`, {
+      contact: {
+        Email: email,
+        LeadSource: 'Footer contact form',
+        custom: {
+          'string--From--Path': pathname,
         },
-      }),
+      },
     })
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(console.log('not working'))
-
     response.status(200).send('it worked')
   } else {
     response.status(401).send('no email')
