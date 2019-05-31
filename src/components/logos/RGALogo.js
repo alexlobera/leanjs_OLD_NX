3efb4047998470f66c31e32f9987dbf19fa7333f
alexlbr <alex.lobera@gmail.com>
2019-05-31 18:32:45 +0100

    updates RGALogo

--- src/components/logos/RGALogo.js
+++ src/components/logos/RGALogo.js
@@ -1,21 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
 import Link from '../navigation/Link'
 
-const SvgLogo = () => <div className="rga-logo" />
-
-const LinkLogo = styled(Link)`
-  width: 160px;
-  display: inline-block;
-  text-decoration: none;
-
-  svg {
-    width: 100%;
-  }
-`
-
 export const RGALogo = () => (
-  <LinkLogo to="/" title="React GraphQL Academy">
-    <SvgLogo />
-  </LinkLogo>
+  <Link to="/" title="React GraphQL Academy">
+    <div className="rga-logo" />
+  </Link>
 )
--- src/components/navigation/menu/DesktopMenu/ItemSubmenu.js
+++ src/components/navigation/menu/DesktopMenu/ItemSubmenu.js
@@ -9,7 +9,7 @@ export const Ul = styled(DefaultUl)`
   background-color: ${blue1(0.75)};
   position: absolute;
   list-style: none;
-  margin: 38px 0 0 -18px !important;
+  margin: 29px 0 0 -18px !important;
   padding: 18px 18px 36px 18px;
   li {
     padding-left: 0;
