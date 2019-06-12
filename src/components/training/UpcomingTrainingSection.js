70df17beef360b55914f74253f9c2742be73ae6e
Richard Moss <richard@englishtutorslive.com>
2019-06-12 15:22:03 +0200

    tab names

--- src/components/training/UpcomingTrainingSection.js
+++ src/components/training/UpcomingTrainingSection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import { formatUTC } from '../utils'
@@ -10,7 +10,19 @@ import Link from '../navigation/Link'
 import { selectUpcomingTrainings } from './UpcomingTrainings'
 import Newsletter from '../elements/Newsletter'
 import { GREY } from '../../config/styles'
-
+import {
+  Tabs,
+  TabList,
+  TabItem,
+  TabContent,
+  ContentItem,
+} from 'src/components/layout/Tabs'
+import {
+  REACT_BOOTCAMP,
+  ADVANCED_REACT,
+  PART_TIME,
+  REACT_NATIVE,
+} from 'src/config/data'
 import CorporateTrainingCard from '../elements/CorporateTrainingCard'
 
 const CorporateCrossSell = styled.div`
@@ -66,72 +78,93 @@ const UpcomingTrainingSection = ({
   type,
   trainings,
   removeAdditionalCTAs = false,
-}) => (
-  <React.Fragment>
-    {curriculum ? (
-      <React.Fragment>
-        <Link to="#upcoming-courses" name="upcoming-courses" />
-        <H3 style={{ marginTop: '1em' }}>Upcoming dates:</H3>
-        <UpcomingTrainings
-          type={type}
-          curriculum={curriculum}
-          trainings={trainings}
-        />
-        <Link to="#upcoming">See all upcoming courses</Link>
-        {!removeAdditionalCTAs && (
-          <React.Fragment>
-            <Row>
-              <Link
-                to="#free-learning-resources"
-                name="free-learning-resources"
-              />
-              <Col md={10}>
-                <Newsletter />
-              </Col>
-            </Row>
-            <Row>
-              <Link to="#corporate-training" name="corporate-training" />
-              <Col md={10}>
-                <CorporateTrainingCard type={type} />
-              </Col>
-            </Row>
-          </React.Fragment>
-        )}
-      </React.Fragment>
-    ) : (
-      <Section>
-        <Grid>
-          <Row>
-            <Col md={10} mdOffset={1}>
-              <H2Ref>
-                Upcoming - All Courses
-                <Link to="#upcoming" name="upcoming">
-                  #
-                </Link>
-              </H2Ref>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={11} mdOffset={1}>
-              <TrainingList>
-                <UpcomingTrainings
-                  type={type}
-                  curriculum={curriculum}
-                  trainings={trainings}
+}) => {
+  const [activeTab, setActiveTab] = useState(REACT_BOOTCAMP)
+
+  return (
+    <React.Fragment>
+      {curriculum ? (
+        <React.Fragment>
+          <Link to="#upcoming-courses" name="upcoming-courses" />
+          <H3 style={{ marginTop: '1em' }}>Upcoming dates:</H3>
+          <UpcomingTrainings
+            type={type}
+            curriculum={curriculum}
+            trainings={trainings}
+          />
+          <Link to="#upcoming">See all upcoming courses</Link>
+          {!removeAdditionalCTAs && (
+            <React.Fragment>
+              <Row>
+                <Link
+                  to="#free-learning-resources"
+                  name="free-learning-resources"
                 />
-                <CorporateCrossSell>
-                  <P>
-                    <strong>Corporate team training</strong>
-                  </P>
-                  <Link to="/react/training/corporate/">Find out more</Link>
-                </CorporateCrossSell>
-              </TrainingList>
-            </Col>
-          </Row>
-        </Grid>
-      </Section>
-    )}
-  </React.Fragment>
-)
+                <Col md={10}>
+                  <Newsletter />
+                </Col>
+              </Row>
+              <Row>
+                <Link to="#corporate-training" name="corporate-training" />
+                <Col md={10}>
+                  <CorporateTrainingCard type={type} />
+                </Col>
+              </Row>
+            </React.Fragment>
+          )}
+        </React.Fragment>
+      ) : (
+        <Section>
+          <Grid>
+            <Row>
+              <Col md={10} mdOffset={1}>
+                <H2Ref>
+                  Upcoming - All Courses
+                  <Link to="#upcoming" name="upcoming">
+                    #
+                  </Link>
+                </H2Ref>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={11} mdOffset={1}>
+                <TrainingList>
+                  <Tabs active={activeTab} onChange={setActiveTab}>
+                    <TabList>
+                      <TabItem name={REACT_BOOTCAMP}>React</TabItem>
+                      <TabItem name={ADVANCED_REACT}>GraphQL</TabItem>
+                      <TabItem name={PART_TIME}>Meetups</TabItem>
+                    </TabList>
+                    <TabContent>
+                      <ContentItem name={REACT_BOOTCAMP}>
+                        <UpcomingTrainings
+                          type={type}
+                          curriculum={curriculum}
+                          trainings={trainings}
+                        />
+                        <CorporateCrossSell>
+                          <P>
+                            <strong>Corporate team training</strong>
+                          </P>
+                          <Link to="/react/training/corporate/">
+                            Find out more
+                          </Link>
+                        </CorporateCrossSell>
+                      </ContentItem>
+
+                      <ContentItem name={ADVANCED_REACT}>something</ContentItem>
+
+                      <ContentItem name={PART_TIME}>something</ContentItem>
+                    </TabContent>
+                  </Tabs>
+                </TrainingList>
+              </Col>
+            </Row>
+          </Grid>
+        </Section>
+      )}
+    </React.Fragment>
+  )
+}
 
 export default UpcomingTrainingSection
