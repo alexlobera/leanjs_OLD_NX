0aec80d2ccb6386cf585ae3fa60bb45942b6c410
Richard Moss <richard@englishtutorslive.com>
2019-06-18 17:14:45 +0200

    Make tempory fix to hardcode links for existing workshops

--- src/components/curriculum/CurriculumReactWorkshops.js
+++ src/components/curriculum/CurriculumReactWorkshops.js
@@ -11,7 +11,6 @@ const CurriculumReactWorkshops = ({
   layout,
   enableToggle,
   isOpen,
-  toggleNavigateTo = `/react/training/workshops`,
   showLinkToCurriculum = false,
   trainings,
 }) => {
@@ -19,7 +18,6 @@ const CurriculumReactWorkshops = ({
   const commonProps = {
     showLinkToCurriculum,
     enableToggle,
-    toggleNavigateTo,
     type,
     isOpen,
     trainings,
@@ -33,6 +31,7 @@ const CurriculumReactWorkshops = ({
         name="styling"
         subTitle="See how React can look gorgeous and encourage design consistency"
         showLinkToCurriculum
+        toggleNavigateTo="/react/training/workshops/design-system-styling-in-react/london/"
       />
 
       <Section
@@ -41,6 +40,7 @@ const CurriculumReactWorkshops = ({
         name="fundamentals"
         subTitle="Learn the basics of React and jumpstart your way into a new coding ecosystem"
         showLinkToCurriculum
+        toggleNavigateTo="/react/training/react-fundamentals/london/"
       />
       <Section
         {...commonProps}
@@ -48,6 +48,7 @@ const CurriculumReactWorkshops = ({
         name="hooksAndSuspense"
         subTitle="Learn 2 of the newest and most exciting features in React"
         showLinkToCurriculum
+        toggleNavigateTo="/react/training/workshops/"
       />
     </React.Fragment>
   )
@@ -59,6 +60,7 @@ const CurriculumReactWorkshops = ({
         name="perfAndFP"
         subTitle="Discover best practice for permonant React apps"
         showLinkToCurriculum
+        toggleNavigateTo="/react/training/workshops/"
       />
       <Section
         {...commonProps}
@@ -66,6 +68,7 @@ const CurriculumReactWorkshops = ({
         name="reactGraphqlClient"
         subTitle="Consume GraphQL APIs in any React application"
         showLinkToCurriculum
+        toggleNavigateTo="/graphql/training/workshops/graphql-apollo-client/london/"
       />
       <Section
         {...commonProps}
@@ -73,6 +76,7 @@ const CurriculumReactWorkshops = ({
         name="testingReact"
         subTitle="Ensure consistent, reliable code across the React ecosystem"
         showLinkToCurriculum
+        toggleNavigateTo="/react/training/workshops/testing-in-react/london/"
       />
       <Section
         {...commonProps}
@@ -80,6 +84,7 @@ const CurriculumReactWorkshops = ({
         name="react-native"
         subTitle="Build upon your React knowledge and create great apps"
         showLinkToCurriculum
+        toggleNavigateTo="/react/training/workshops/"
       />
     </React.Fragment>
   )
