7e752c24080f9b67c8e3f6f69732aeab3e6f8b2e
alexlbr <alex.lobera@gmail.com>
2019-06-22 19:27:22 +0200

    updates curriculum.js

--- src/pages/react/curriculum.js
+++ src/pages/react/curriculum.js
@@ -452,8 +452,8 @@ class ReactCurriculum extends React.Component {
                           <ContentItem name={PART_TIME}>
                             <P>
                               <strong>
-                                On completion of the React Bootcamp each student
-                                will:
+                                On completion of the React part-time course each
+                                student will:
                               </strong>
                             </P>
                             <Ul>
