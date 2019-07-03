d2a5a319b003ba9580a7371219788dfcbeedd493
Richard Moss <richard@englishtutorslive.com>
2019-07-03 19:53:05 +0200

    Add tracking for SectionCTA (Full curriculum button clicks) and update snapshots

--- src/components/curriculum/SectionCTA.js
+++ src/components/curriculum/SectionCTA.js
@@ -1,6 +1,11 @@
+import React from 'react'
 import styled from 'styled-components'
 
-const SectionCTA = styled.div`
+const SectionCTA = ({ children }) => (
+  <SectionCTAstyles className="curriculum-cta">{children}</SectionCTAstyles>
+)
+
+const SectionCTAstyles = styled.div`
   padding-top: 50px;
   padding-bottom: 25px;
 `
