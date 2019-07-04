fbc6acdf77cd08df24c3cadb9eb14e9397e1b364
alexlbr <alex.lobera@gmail.com>
2019-07-04 08:44:59 +0200

    updates default gatsby-config metas

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'React GraphQL Academy',
+    title: 'React & GraphQL Expert Training | React GraphQL Academy',
     description:
-      'React, Redux & GraphQL bootcamps and trainings - Take your dev career further by mastering the React ecosystem',
-    keywords:
-      'React, React Experts, React Instructor, Build React App, Understanding React JS, Javascript Training, Redux, React and Redux, Redux Expert, Learn Redux, Master Redux, Graphql Instructors, Graphql Workshop, React Redux, React Expert, React Trainer, Code Camp, Javascript Bootcamp, Coding Bootcamp, Learn React JS, GraphQL Bootcamp, Graphql Learn, Graphql Company Training, Graphql private training, React Javascript, React Tutorial, Coding Bootcamp, Graphql Expert, Graphql Instructor, Dev Bootcamp, Learn React, React with Bootstrap, Graphql Training, Learn GraphQL, React Bootcamp, React Consultants, React JS Developer, React Training, React class, Learn to Code, Graphql Experts, Graphql onsite training, Graphql trainier, React Graphql, React Training, Advanced React and Redux, Graphql React, Master GraphQL, GraphQL, Redux Training, Graphql Course, Graphql on-site training, How to Graphql',
+      'Looking for React and GraphQL expert training? React GraphQL Academy offers in-person real-world training by our experts. Contact us now!',
     siteUrl: `https://www.reactgraphql.academy`,
   },
   plugins: [
