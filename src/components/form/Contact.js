08359063b0908a3a66f622b79f6956d03f805455
Richard Moss <richard@englishtutorslive.com>
2019-07-04 14:28:31 +0200

    Add mailto text update

--- src/components/form/Contact.js
+++ src/components/form/Contact.js
@@ -55,7 +55,7 @@ const ContactForm = props => {
           <P>
             The best way to contact us is by emailing us at{' '}
             <Link
-              to="mailto:hello@reactgrahql.academy"
+              to="mailto:hello@reactgrahql.academy?subject=Course%20Query&body=Hi%20RGA%20team!"
               className="footer-contact-us-mailto"
             >
               hello@reactgraphql.academy
