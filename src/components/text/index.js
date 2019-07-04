3102ddbaff073f9265d71b3ee39383dd9d860ca2
alexlbr <alex.lobera@gmail.com>
2019-07-04 09:13:26 +0200

    refactores the fontColor function in Card and uses it in Footer too

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -1,6 +1,29 @@
+import { styleChildLinkColor } from '../navigation/Link'
 export { H1, H1Ref, H2, H2Ref, H3, H4, H5 } from './H'
 export { default as Blockquote } from './Blockquote'
 export { default as Label } from './Label'
 export { default as P } from './P'
 export { default as Span } from './Span'
 export { default as Hr } from './Hr'
+export const fontColor = color => `
+  color: ${color};
+  h2 {
+    color: ${color};
+  }
+  h3 {
+    color: ${color};
+  }
+  h4 {
+    color: ${color};
+  }
+  h5 {
+    color: ${color};
+  }
+  p {
+    color: ${color};
+  }
+  span {
+    color: ${color};
+  }
+  ${styleChildLinkColor(color)}
+`
