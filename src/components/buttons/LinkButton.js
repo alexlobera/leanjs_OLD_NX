8db3b50b42b98a211389c88ddda825869a34b35f
alexlbr <alex.lobera@gmail.com>
2019-07-20 08:25:27 +0100

    removes unused import from LinkButton

--- src/components/buttons/LinkButton.js
+++ src/components/buttons/LinkButton.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { space } from 'styled-system'
 
 import Link from '../navigation/Link'
 import {
