294b4200fb2a0faa91500ec3808621edc7a3061b
alexlbr <alex.lobera@gmail.com>
2019-07-20 09:09:32 +0100

    adds variant to the Alert component

--- src/components/elements/Alert.js
+++ src/components/elements/Alert.js
@@ -1,16 +1,32 @@
+import React from 'react'
 import styled from 'styled-components'
 
-import { PINK } from '../../config/styles'
+import { PINK, GREY } from '../../config/styles'
 import Box from '../layout/Box'
 
-const Alert = styled(Box)`
-  ${props => (props.danger ? `background-color: ${PINK};` : null)}
-`
+const StyledAlert = styled(Box)``
+
+const Alert = ({ variant, ...rest }) => (
+  <StyledAlert {...(variant ? alertVariants[variant] : {})} {...rest} />
+)
+
+const alertVariants = {
+  default: {
+    border: '1px solid',
+    bordercolor: GREY,
+  },
+  danger: {
+    backgroundColor: PINK,
+    fontWeight: 'bold',
+  },
+}
+
 Alert.defaultProps = {
   my: 1,
   pl: 1,
   pr: 1,
   py: 3,
+  variant: 'default',
 }
 
 export default Alert
