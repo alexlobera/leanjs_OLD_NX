9045fa7c85a8c939d479a2f845fdf54c72e0b942
alexlbr <alex.lobera@gmail.com>
2019-07-21 18:01:37 +0100

    removes custom media query from CallToActionRow.js

--- src/components/layout/CallToActionRow.js
+++ src/components/layout/CallToActionRow.js
@@ -1,18 +1,10 @@
 import styled from 'styled-components'
 
-import { SCREEN_XS_MAX } from '../utils'
 import { Z_INDEX_MEDIUM } from '../../config/styles'
 import { Row } from './Grid'
 import Box from './Box'
 
-export const CallToActionRow = styled(Box)`
-  @media (max-width: ${SCREEN_XS_MAX}) {
-    a {
-      display: block;
-      margin: 5px 0;
-    }
-  }
-`
+export const CallToActionRow = styled(Box)``
 CallToActionRow.defaultProps = {
   mb: [0, -25],
   position: 'relative',
