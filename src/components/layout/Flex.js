16c5f888a26f3a0e8c7a4211ad32d29078d03190
alexlbr <alex.lobera@gmail.com>
2019-07-21 18:04:45 +0100

    refactores Flex componenent

--- src/components/layout/Flex.js
+++ src/components/layout/Flex.js
@@ -1,25 +1,12 @@
 import styled from 'styled-components'
-import {
-  flexWrap,
-  flexDirection,
-  alignItems,
-  justifyContent,
-} from 'styled-system'
+import { flexbox } from 'styled-system'
 import Box from './Box'
 
-const Flex = styled(Box)(
-  {
-    display: 'flex',
-  },
-  flexWrap,
-  flexDirection,
-  alignItems,
-  justifyContent
-)
-
-// Alex, refactor the previous code to use this https://styled-system.com/api/#flexbox
-// const Flex = styled(Box)`
-//  ${flexbox}
-// `
+const Flex = styled(Box)`
+  ${flexbox}
+`
+Flex.defaultProps = {
+  display: 'flex',
+}
 
 export default Flex
