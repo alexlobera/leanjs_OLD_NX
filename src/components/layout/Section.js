70daacd8906bd80550ff8f49bf227ddb31323a8c
alexlbr <alex.lobera@gmail.com>
2019-07-28 15:44:14 +0200

    updates Col component and creates ColSection

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { DARK_BLUE, DARK_GREY } from '../../config/styles'
 import Box from './Box'
-import Grid from './Grid'
+import Grid, { Col, Row } from './Grid'
 
 export const MOB_SECTION_MARGIN_Y = 5
 
@@ -46,7 +46,62 @@ TopSection.defaultProps = {
   position: 'relative',
   mt: [0, -150],
 }
-
 TopSection.displayName = 'TopSection'
+
+export const ColSection = ({ variant, col, col2, ...rest }) => {
+  const variantProps = colSectionVariantProps[variant] || {}
+  const col1Props =
+    col && col2 ? variantProps.col1 || {} : { md: 10, mdOffset: 1 }
+  const Col1 = <Col {...col1Props}>{col}</Col>
+  const Col2 = col2 && <Col {...(variantProps.col2 || {})}>{col2}</Col>
+
+  return (
+    <Section {...rest}>
+      <Row>
+        {Col1}
+        {Col2}
+      </Row>
+    </Section>
+  )
+}
+
+const colSectionVariantProps = {
+  thin: {
+    col1: {
+      md: 5,
+      mdOffset: 1,
+    },
+    col2: {
+      md: 4,
+      mdOffset: 1,
+      mt: [5, 0],
+    },
+  },
+  thinLeft: {
+    col1: {
+      md: 5,
+      mdOffset: 1,
+    },
+    col2: {
+      md: 5,
+      mdOffset: 1,
+      mt: [5, 0],
+    },
+  },
+  thinRight: {
+    col1: {
+      md: 6,
+    },
+    col2: {
+      md: 4,
+      mdOffset: 1,
+      mt: [5, 0],
+    },
+  },
+}
+
+ColSection.defaultProps = {
+  variant: 'thin',
+}
 
 export default Section
