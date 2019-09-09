002154c5f707fddd627d034c7b0b0c854df81a02
alexlbr <alex.lobera@gmail.com>
2019-09-09 09:23:19 +0100

    removes commented out code

--- src/components/blog/PostCard.js
+++ src/components/blog/PostCard.js
@@ -64,33 +64,3 @@ export const query = graphql`
 `
 
 export default PostCard
-
-// const PostCard = ({ post, imageProps = {} }) => (
-//   <Card small variant="secondary" mb={5}>
-//     <Link to={`${post.fields.slug}`} className="articles-summary">
-//       <Image
-//         {...imageProps}
-//         src={
-//           post.frontmatter.imageUrl ||
-//           (post.frontmatter.imageSrc &&
-//             post.frontmatter.imageSrc.childImageSharp.fluid.src)
-//         }
-//         alt={formatPostTitle(post.frontmatter.title)}
-//         mb={0}
-//       />
-//     </Link>
-//     <Box p={2}>
-//       <Link to={`${post.fields.slug}`} className="articles-summary">
-//         <H3>{formatPostTitle(post.frontmatter.title)}</H3>
-//       </Link>
-//       <P>{post.excerpt}</P>
-//       <P>
-//         <Link to={`${post.fields.slug}`} className="articles-summary">
-//           Read more
-//         </Link>
-//       </P>
-//     </Box>
-//   </Card>
-// )
-
-// export default PostCard
