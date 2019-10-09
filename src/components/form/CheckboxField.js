0ed404107801c20fd2965276c9c9bbd9ceac4c20
alexlbr <alex.lobera@gmail.com>
2019-10-09 18:55:56 +0200

    removes commented out code

--- src/components/form/CheckboxField.js
+++ src/components/form/CheckboxField.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Field } from 'react-final-form'
-// import InputField from './InputField'
+
 import Label from '../text/Label'
 
 const defaultSize = 2
