a2a33be3b8a476a326509383658330a333d6647f
alexlbr <alex.lobera@gmail.com>
2019-12-23 08:00:53 +0000

    update adv curriculum workshop

--- src/components/curriculum/workshops/CurriculumAdvReactPatterns/LearningObjectivesList.js
+++ src/components/curriculum/workshops/CurriculumAdvReactPatterns/LearningObjectivesList.js
@@ -4,9 +4,10 @@ import { Li } from '../../../layout/Ul'
 const LearningObjectivesList = () => (
   <React.Fragment>
     <Li>
-      Be able to use advanced React patterns & functional programming to improve
-      performance and code quality.
+      Be able to use advanced patterns & functional programming to improve code
+      quality
     </Li>
+    <Li>Understand how to improve absolute and perceived performance</Li>
     <Li>Understand basic theoretical concepts of React</Li>
   </React.Fragment>
 )
