4f1ad1dd7f943ef200fa69cd211d719b9cc831a6
alexlbr <alex.lobera@gmail.com>
2020-02-13 15:01:48 +0000

    move website to www workspace

--- cypress/integration/navigation.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Navigation', function() {
-  it("clicking '1-week bootcamp' navigates to a correct url", function() {
-    cy.visit('/react/training/')
-
-    cy.contains('React Courses').click()
-
-    cy.url().should('include', '/react/training/')
-  })
-})
