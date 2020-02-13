1d471c67dc0c8eb5d04d417546c3482a1459ec2d
alexlbr <alex.lobera@gmail.com>
2020-02-13 11:27:10 +0000

    add box package

---
+++ packages/box/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+import {
+  space,
+  color,
+  typography,
+  border,
+  shadow,
+  layout,
+  position,
+  compose,
+} from 'styled-system'
+
+const css = compose(space, color, typography, border, shadow, layout, position)
+
+const StyledBox = React.memo(
+  styled(({ sx, variant, box: Component = 'div', ...rest }) => (
+    <Component {...rest} />
+  ))(({ sx, theme }) => css({ ...sx, theme }))
+)
+
+const Box = React.forwardRef(({ sx = {}, ...rest }, ref) => (
+  <StyledBox
+    sx={{
+      fontFamily: 'barlow',
+      fontWeight: 'normal',
+      color: 'text',
+      ...sx,
+    }}
+    {...rest}
+    ref={ref}
+  />
+))
+
+Box.displayName = 'Box'
+
+export default React.memo(Box)
+export { Box }
