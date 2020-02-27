29eacd1b46d8510636a990d735a2ef9edc40d708
Tiago Gomes <t.dfgomes@icloud.com>
2020-02-27 15:16:55 +0000

    wip: add alert to packages

---
+++ packages/alert/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Box from "@leanjs/Box";
+
+const Alert = ({ variant = "default", sx = {}, ...rest }) => (
+  <Box
+    sx={{
+      my: 1,
+      pl: 1,
+      pr: 1,
+      py: 3,
+      ...(alertVariants[variant] || {}),
+      ...sx
+    }}
+    {...rest}
+  />
+);
+
+const alertVariants = {
+  default: {
+    border: "1px solid",
+    bordercolor: "text"
+  },
+  danger: {
+    backgroundColor: "danger",
+    fontWeight: "bold"
+  }
+};
+
+export default Alert;
