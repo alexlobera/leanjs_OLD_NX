55b2c9c9feeb70ad1de1f0e4ecb3028f968f39cf
alexlbr <alex.lobera@gmail.com>
2020-02-29 00:27:50 +0000

    update packages/box/package.json and remove commented out code from  www/src/components/elements/Alert.js

--- www/src/components/elements/Alert.js
+++ www/src/components/elements/Alert.js
@@ -1,28 +1,2 @@
-// import React from 'react'
 import Alert from '@leanjs/alert'
 export default Alert
-
-// const Alert = ({ variant = 'default', sx = {}, ...rest }) => (
-//   <Box
-//     sx={{
-//       my: 1,
-//       pl: 1,
-//       pr: 1,
-//       py: 3,
-//       ...(alertVariants[variant] || {}),
-//       ...sx,
-//     }}
-//     {...rest}
-//   />
-// )
-
-// const alertVariants = {
-//   default: {
-//     border: '1px solid',
-//     bordercolor: 'text',
-//   },
-//   danger: {
-//     backgroundColor: 'danger',
-//     fontWeight: 'bold',
-//   },
-// }
