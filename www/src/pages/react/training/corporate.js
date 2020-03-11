4f402d33e6dd36c760bb1613c9891397ed5c064c
alexlbr <alex.lobera@gmail.com>
2020-03-11 00:25:51 +0000

    update react and gql corp pages

--- www/src/pages/react/training/corporate.js
+++ www/src/pages/react/training/corporate.js
@@ -13,10 +13,11 @@ import Header from 'src/components/layout/Header'
 import { CallToActionRow } from 'src/components/layout/CallToActionRow'
 import { Segment } from 'src/components/elements'
 import CurriculumCorporateReact from 'src/components/curriculum/CurriculumCorporateReact'
-import LeanJSsprints from 'src/components/LeanJSsprints'
 import { createMetas } from 'src/components/utils'
 import { WHY_REACTJS_ACADEMY } from 'src/config/images.js'
 import { TECH_REACT } from 'src/config/data'
+import { getPostsFromNodes } from 'src/components/blog/utils'
+import BlogSection from 'src/components/blog/BlogSection'
 
 const metas = {
   title: 'React Corporte Training | React GraphQL Academy',
@@ -26,91 +27,115 @@ const metas = {
   type: 'website',
 }
 
-const CorporateReactTraining = ({ path, trainings }) => (
-  <React.Fragment>
-    <Helmet
-      title={metas.title}
-      meta={[
-        {
-          name: 'description',
-          content: metas.description,
-        },
-      ]}
-    >
-      {createMetas(metas)}
-    </Helmet>
-    <Header
-      breadcrumbPath={[
-        { to: '/', label: 'Home' },
-        {
-          to: '/react',
-          label: 'React',
-        },
-        {
-          to: '/react/training/',
-          label: 'Training',
-        },
-        {
-          to: path,
-          label: 'Corporate',
-        },
-      ]}
-      tech={TECH_REACT}
-      titleLines={['Corporate React team training']}
-      subtitle="Private team training, located in your offices anywhere in the world, based on our proven React curriculum."
-      links={[
-        { text: 'Training schedule', to: '#schedule' },
-        { text: 'How tailored', to: '#how-tailored' },
-        { text: 'Pricing', to: '#pricing' },
-        { text: 'Previous clients', to: '#previous-clients' },
-        { text: 'Custom training', to: '#custom-training' },
-      ]}
-      bgImageName={CORP_TRAINING}
-    />
-    <TopSection>
-      <CallToActionRow>
-        <Col mdOffset={1} md={4}>
-          <LinkButton
-            variant="primary"
-            to="https://firebasestorage.googleapis.com/v0/b/reactgraphqlacademy.appspot.com/o/pdfs%2FCorporate%20Team%20Training%20with%20React%20GraphQL%20Academy.pdf?alt=media&"
-            children="Team Training - Key Facts (PDF)"
-          />
-        </Col>
-        <Col mdOffset={1} md={6} />
-      </CallToActionRow>
-      <a name="schedule" />
-      <Segment>
-        <CurriculumCorporateReact />
-      </Segment>
-    </TopSection>
-    <Section>
-      <Row>
-        <Col md={5} mdOffset={1} order={[2, 1]}>
-          <a name="previous-clients" />
-          <AttendeeQuote
-            quote="It's really hard to find good advanced tech training. In house, you standardise the knowledge [in a team] and start doing things the right way."
-            fullname="Eber Herrera"
-            job="Technical Director"
-            company="Xing"
-            youtubeId="TAtsPib9P9w"
-            ctaButton="How Training Keeps Xing 
+const CorporateReactTraining = ({ path, trainings, data }) => {
+  const posts = getPostsFromNodes({
+    sanityNodes: data.allSanityPost.nodes,
+  }).slice(0, 3)
+
+  return (
+    <React.Fragment>
+      <Helmet
+        title={metas.title}
+        meta={[
+          {
+            name: 'description',
+            content: metas.description,
+          },
+        ]}
+      >
+        {createMetas(metas)}
+      </Helmet>
+      <Header
+        breadcrumbPath={[
+          { to: '/', label: 'Home' },
+          {
+            to: '/react',
+            label: 'React',
+          },
+          {
+            to: '/react/training/',
+            label: 'Training',
+          },
+          {
+            to: path,
+            label: 'Corporate',
+          },
+        ]}
+        tech={TECH_REACT}
+        titleLines={['Corporate React team training']}
+        subtitle="Private team training, located in your offices anywhere in the world, based on our proven React curriculum."
+        links={[
+          { text: 'Training schedule', to: '#schedule' },
+          { text: 'How tailored', to: '#how-tailored' },
+          { text: 'Pricing', to: '#pricing' },
+          { text: 'Previous clients', to: '#previous-clients' },
+          { text: 'Custom training', to: '#custom-training' },
+        ]}
+        bgImageName={CORP_TRAINING}
+      />
+      <TopSection>
+        <CallToActionRow>
+          <Col mdOffset={1} md={4}>
+            <LinkButton
+              variant="primary"
+              to="https://firebasestorage.googleapis.com/v0/b/reactgraphqlacademy.appspot.com/o/pdfs%2FCorporate%20Team%20Training%20with%20React%20GraphQL%20Academy.pdf?alt=media&"
+              children="Team Training - Key Facts (PDF)"
+            />
+          </Col>
+          <Col mdOffset={1} md={6} />
+        </CallToActionRow>
+        <a name="schedule" />
+        <Segment>
+          <CurriculumCorporateReact />
+        </Segment>
+      </TopSection>
+      <Section>
+        <Row>
+          <Col md={5} mdOffset={1} order={[2, 1]}>
+            <a name="previous-clients" />
+            <AttendeeQuote
+              quote="It's really hard to find good advanced tech training. In house, you standardise the knowledge [in a team] and start doing things the right way."
+              fullname="Eber Herrera"
+              job="Technical Director"
+              company="Xing"
+              youtubeId="TAtsPib9P9w"
+              ctaButton="How Training Keeps Xing 
               Tech-Relevant (Blog)"
-            ctaUrl="/blog/5-reasons-why-advanced-dev-training-will-keep-your-company-tech-relevant/"
-          />
-        </Col>
-        <Col md={4} lgOffset={1} order={[1, 2]}>
-          <H2>We have delivered Corporate training to almost any industry</H2>
-          <TrustedByLogoList2 />
-        </Col>
-      </Row>
-    </Section>
-    <TrustedBySection type="contact" showContent />
-    <Section>
-      <a name="custom-training" />
-      <LeanJSsprints />
-    </Section>
-    <UpcomingTrainingSection trainings={trainings} />
-  </React.Fragment>
-)
+              ctaUrl="/blog/5-reasons-why-advanced-dev-training-will-keep-your-company-tech-relevant/"
+            />
+          </Col>
+          <Col md={4} lgOffset={1} order={[1, 2]}>
+            <H2>We have delivered Corporate training to almost any industry</H2>
+            <TrustedByLogoList2 />
+          </Col>
+        </Row>
+      </Section>
+      <TrustedBySection type="contact" showContent />
+      {/* <Section>
+        <a name="custom-training" />
+        <LeanJSsprints />
+      </Section> */}
+      <BlogSection title="From our blog" posts={posts} />
+      <UpcomingTrainingSection trainings={trainings} />
+    </React.Fragment>
+  )
+}
+
+export const query = graphql`
+  query {
+    allSanityPost(
+      limit: 3
+      filter: {
+        category: { in: ["react", "blog"] }
+        tags: { elemMatch: { name: { in: ["business"] } } }
+      }
+      sort: { fields: [order, publishedAt], order: [ASC, DESC] }
+    ) {
+      nodes {
+        ...SanityPostItemFragment
+      }
+    }
+  }
+`
 
 export default CorporateReactTraining
