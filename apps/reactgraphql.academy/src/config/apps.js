100da1fcdd72e3eaccf0c9e5ac4a2fd4fa81d6a9
alexlbr <alex.lobera@gmail.com>
2020-07-06 13:42:35 +0100

    online site work in progress

--- apps/reactgraphql.academy/src/config/apps.js
+++ apps/reactgraphql.academy/src/config/apps.js
@@ -1,4 +1,4 @@
 export const UPMENTORING_API_URL = 'https://api2.upmentoring.com/graphql';
-// export const UPMENTORING_API_URL = 'https://api-staging.upmentoring.com/graphql'
+/// export const UPMENTORING_API_URL = 'https://api-staging.upmentoring.com/graphql'
 export const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY;
 export const SENTRY_DSN = process.env.SENTRY_DSN;
