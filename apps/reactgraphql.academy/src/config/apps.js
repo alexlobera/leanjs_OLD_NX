27dcff986e9ff1a3245198571e8fa567a189993a
alexlbr <alex.lobera@gmail.com>
2020-07-06 14:44:36 +0100

    update env Gatsby variables

--- apps/reactgraphql.academy/src/config/apps.js
+++ apps/reactgraphql.academy/src/config/apps.js
@@ -1,4 +1,4 @@
 export const UPMENTORING_API_URL = 'https://api2.upmentoring.com/graphql';
 /// export const UPMENTORING_API_URL = 'https://api-staging.upmentoring.com/graphql'
-export const STRIPE_PUBLIC_KEY = process.env.STRIPE_PUBLIC_KEY;
-export const SENTRY_DSN = process.env.SENTRY_DSN;
+export const STRIPE_PUBLIC_KEY = process.env.GATSBY_STRIPE_PUBLIC_KEY;
+export const SENTRY_DSN = process.env.GATSBY_SENTRY_DSN;
