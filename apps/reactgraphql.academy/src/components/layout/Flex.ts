bdb397e5856890871eacb82d4cae6c020dfb3fbe
alexlbr <alex.lobera@gmail.com>
2020-07-08 00:25:14 +0100

    remove commented out code in components/layout/Flex.ts

--- apps/reactgraphql.academy/src/components/layout/Flex.ts
+++ apps/reactgraphql.academy/src/components/layout/Flex.ts
@@ -1,20 +1,3 @@
 import { Flex } from '@leanjs/ui-core';
 
 export default Flex;
-
-// import React from 'react';
-// import { css } from 'styled-components';
-// import { flexbox } from 'styled-system';
-// import Box from './Box';
-
-// const Flex = ({ sx = {}, ...rest }) => (
-//   <Box
-//     css={css`
-//       ${({ theme, sx }) => flexbox({ theme, ...sx })}
-//     `}
-//     sx={{ display: 'flex', ...sx }}
-//     {...rest}
-//   />
-// );
-
-// export default React.memo(Flex);
