4b9f83928dbae2b49c3bc69e04e0926987a6665a
alexlbr <alex.lobera@gmail.com>
2020-07-08 00:26:39 +0100

    remove commented out code in package leanui box

--- packages/ui-core/src/box/index.tsx
+++ packages/ui-core/src/box/index.tsx
@@ -1,32 +1,6 @@
 import React, { ReactNode, FunctionComponent } from 'react';
 import styled from 'styled-components';
-// import {
-//   space,
-//   SpaceProps,
-//   color,
-//   ColorProps,
-//   typography,
-//   TypographyProps,
-//   border,
-//   BorderProps,
-//   shadow,
-//   ShadowProps,
-//   layout,
-//   LayoutProps,
-//   position,
-//   PositionProps,
-//   compose,
-// } from 'styled-system';
-
 import { css, CssProps as SxProp } from '@styled-system/css';
-
-// export type SxProp = SpaceProps &
-//   ColorProps &
-//   TypographyProps &
-//   BorderProps &
-//   ShadowProps &
-//   LayoutProps &
-//   PositionProps;
 
 export type BoxProps<T = {}> = T & {
   sx?: SxProp;
@@ -39,21 +13,6 @@ export type StyledBoxProps<T = {}> = BoxProps<T> & {
   variant?: string;
   ref?: React.Ref<HTMLElement>;
 };
-
-// const css = compose(
-//   {
-//     boxSizing: 'border-box',
-//     margin: 0,
-//     minWidth: 0,
-//   },
-//   space,
-//   color,
-//   typography,
-//   border,
-//   shadow,
-//   layout,
-//   position
-// );
 
 const StyledBox: FunctionComponent<StyledBoxProps> = React.memo(
   styled(({ sx, variant, box: Component = 'div', ...rest }) => (
