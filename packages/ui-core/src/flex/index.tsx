215967a4f1b547e43fb9949cded7b65b82df4b53
alexlbr <alex.lobera@gmail.com>
2020-07-08 00:27:55 +0100

    remove commented out code in package leanui flex

--- packages/ui-core/src/flex/index.tsx
+++ packages/ui-core/src/flex/index.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
-// import styled from 'styled-components';
-// import { flexbox } from '@styled-system/flexbox';
 import { Box } from '../box';
-
-// const Flex = styled(Box)`
-//   background-color: red;
-// `;
-// Flex.defaultProps = {
-//   sx: { display: 'flex' },
-// };
-// Flex.displayName = 'Flex';
 
 const Flex = ({ sx, ...rest }) => (
   <Box sx={{ display: 'flex', ...sx }} {...rest} />
