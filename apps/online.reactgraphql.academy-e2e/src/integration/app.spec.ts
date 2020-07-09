32521c0787b1322698caabda9a4e740302e5d716
alexlbr <alex.lobera@gmail.com>
2020-07-09 11:17:18 +0100

    update online e2e test

--- apps/online.reactgraphql.academy-e2e/src/integration/app.spec.ts
+++ apps/online.reactgraphql.academy-e2e/src/integration/app.spec.ts
@@ -4,10 +4,6 @@ describe('online.reactgraphql.academy', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to online.reactgraphql.academy!');
+    getGreeting().contains('Welcome');
   });
 });
--- apps/online.reactgraphql.academy/src/pages/index.tsx
+++ apps/online.reactgraphql.academy/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Page() {
-  return <h1>home</h1>;
+  return <h1>Welcome</h1>;
 }
 
 export default Page;
