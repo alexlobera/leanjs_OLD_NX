3fa7eb8094052e64c7cac307d0091dac4a2468a3
alexlbr <alex.lobera@gmail.com>
2020-07-10 18:54:25 +0100

    add layout component to online academy

---
+++ apps/online.reactgraphql.academy/src/components/Layout.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+function Layout({ children }) {
+  return (
+    <React.Fragment>
+      <CSSReset />
+      <Helmet
+        htmlAttributes={{
+          lang: 'en',
+        }}
+        title={data && data.site && data.site.siteMetadata.title}
+        meta={[
+          {
+            name: 'description',
+            content: data && data.site && data.site.siteMetadata.description,
+          },
+        ]}
+        link={[
+          {
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: `${favicon}`,
+          },
+        ]}
+      ></Helmet>
+      {children}
+      {/* <Footer />
+      <AcceptCookies /> */}
+    </React.Fragment>
+  );
+}
+
+export default Layout;
