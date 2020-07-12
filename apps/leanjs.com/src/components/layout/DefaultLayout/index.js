ecfaf9a51a3f36d1500776c512c4ffa4b4c23cc5
alexlbr <alex.lobera@gmail.com>
2020-07-12 13:46:19 +0100

    remove leanjs reset css

--- apps/leanjs.com/src/components/layout/DefaultLayout/index.js
+++ apps/leanjs.com/src/components/layout/DefaultLayout/index.js
@@ -4,7 +4,6 @@ import { Helmet } from 'react-helmet';
 import favicon from './favicon.ico';
 import Footer from '../Footer';
 import './index.css';
-// import './reset.css';
 import 'normalize.css';
 
 const Layout = ({ children }) => (
