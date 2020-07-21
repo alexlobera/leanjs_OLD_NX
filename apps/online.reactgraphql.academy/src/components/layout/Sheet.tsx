0648fd217ff1aac0bc604033872a465f047b96c4
alexlbr <alex.lobera@gmail.com>
2020-07-21 16:07:47 +0100

    updat Sheet padding

--- apps/online.reactgraphql.academy/src/components/layout/Sheet.tsx
+++ apps/online.reactgraphql.academy/src/components/layout/Sheet.tsx
@@ -5,7 +5,7 @@ import { Grid, Box, Card } from '.';
 export default function Sheet(props) {
   return (
     <Grid as={Card} columns={12} sx={{ p: 0 }}>
-      <Box sx={{ gridColumn: ['1/ -1', '2  / -2'], pb: 8, pt: 8 }}>
+      <Box sx={{ gridColumn: ['1/ -1', '1/ -1', '2  / -2'], pb: 8, pt: 8 }}>
         {props.children}
       </Box>
     </Grid>
