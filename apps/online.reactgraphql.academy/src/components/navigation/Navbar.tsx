8b0a8afb1e3d3a647cb3eaa37d313c81e6b88b23
alexlbr <alex.lobera@gmail.com>
2020-08-14 10:42:45 +0100

    fix typo online.reactgraphql.academy/src/components/navigation/Navbar.tsx

--- apps/online.reactgraphql.academy/src/components/navigation/Navbar.tsx
+++ apps/online.reactgraphql.academy/src/components/navigation/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar: LeanComponent<HeadeProps> = ({
         >
           <Flex>
             <Link
-              className="avigation-logo"
+              className="navigation-logo"
               to="/"
               title="React GraphQL Academy Online"
             >
