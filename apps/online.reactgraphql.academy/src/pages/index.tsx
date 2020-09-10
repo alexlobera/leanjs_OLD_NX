ab103da87bbe95e9be95e80d8df3ff26540d0dfa
alexlbr <alex.lobera@gmail.com>
2020-09-10 14:03:34 +0100

    update online home page

--- apps/online.reactgraphql.academy/src/pages/index.tsx
+++ apps/online.reactgraphql.academy/src/pages/index.tsx
@@ -85,14 +85,14 @@ function Page({ data }: PageProps) {
               data.upmentoring.trainings.edges.map(({ node }: any) => {
                 return (
                   <CourseCard
-                    fixedImage={thumbnails.get(node.slug).fixed}
+                    fixedImage={thumbnails.get(node.slug)?.fixed}
                     course={node}
                   />
                 );
               })}
             <CourseCard
               isAvailable={false}
-              fixedImage={thumbnails.get('graphql-foundation').fixed}
+              fixedImage={thumbnails.get('graphql-foundation')?.fixed}
               course={{
                 slug: 'graphql-foundation',
                 title: 'GraphQL Foundation',
@@ -101,7 +101,7 @@ function Page({ data }: PageProps) {
               }}
             />
             <CourseCard
-              fixedImage={thumbnails.get('advanced-react').fixed}
+              fixedImage={thumbnails.get('advanced-react')?.fixed}
               isAvailable={false}
               course={{
                 title: 'Avanced React',
