ec2e8fc68d43ad5941fdcec493cf84bfab4b4d1b
alexlbr <alex.lobera@gmail.com>
2020-09-24 10:50:14 +0100

    format upmentoring-e2e

--- apps/upmentoring-e2e/src/integration/app.spec.ts
+++ apps/upmentoring-e2e/src/integration/app.spec.ts
@@ -4,7 +4,8 @@ describe('upmentoring', () => {
   beforeEach(() => cy.visit('/login'));
 
   it('should display welcome message', () => {
-
-    cy.get('label').contains(`Enter your email address and we'll email you a login link:`);
+    cy.get('label').contains(
+      `Enter your email address and we'll email you a login link:`
+    );
   });
 });
