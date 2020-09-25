357011cd208a41057d249fc1afac3034f596be1e
alexlbr <alex.lobera@gmail.com>
2020-09-25 12:25:35 +0100

    formatting

--- apps/upmentoring/src/App/Components/Root.tsx
+++ apps/upmentoring/src/App/Components/Root.tsx
@@ -53,7 +53,11 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
 const Root = () => (
   <Router>
     <ToastContainer />
-    <MagicProvider magicKey={process.env.NX_MAGIC_LINK_PK_KEY} requirePreSignup={requireSignup} login={login}>
+    <MagicProvider
+      magicKey={process.env.NX_MAGIC_LINK_PK_KEY}
+      requirePreSignup={requireSignup}
+      login={login}
+    >
       {({ getToken }: any) => {
         const authLink = setContext(async (_, { headers, ...context }) => {
           const token = await getToken();
