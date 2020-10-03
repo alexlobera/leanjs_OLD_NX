d7c35c59e0d547ea62ac5afe66f68daeca03a865
alexlbr <alex.lobera@gmail.com>
2020-10-03 16:35:42 +0100

    feedback 8.4

--- apps/online.reactgraphql.academy/gatsby-node.js
+++ apps/online.reactgraphql.academy/gatsby-node.js
@@ -38,6 +38,7 @@ exports.createPages = async ({ graphql, actions }) => {
                         transcript
                       }
                       asset {
+                        isPrivate
                         playback {
                           policy
                         }
